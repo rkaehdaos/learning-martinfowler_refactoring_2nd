a2ed727e89d3d5f248397745a4472252413aae9a
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-01-21 17:46:04 +0900

    [#1] 스위치문 메서드문 추출 - 2
    - 바뀌지 않는 값 2개만 인자로 받도록 수정

--- ch1/statement.spec.js
+++ ch1/statement.spec.js
@@ -1,7 +1,7 @@
 let invoiceData = require('./invoices.json');
 let playData = require('./plays.json');
 const {statement} = require("./statement.js");
-test('ㅇㅇㅇ', () => {
+test('ch1', () => {
     expect(statement(invoiceData, playData)).toContain("총액: $1,730.00");
     expect(statement(invoiceData, playData)).toContain("적립 포인트: 47점");
 
