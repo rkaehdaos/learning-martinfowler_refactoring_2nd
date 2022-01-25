c5f5f38c51f61a57bb6a2a50e61d768e291dfae9
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-01-25 14:55:41 +0900

    [#1] p58 total* 2개 메서드 statement로

--- ch1/statement.js
+++ ch1/statement.js
@@ -3,6 +3,8 @@ module.exports = {
         const statementData = {};
         statementData.customer = invoice.customer;
         statementData.performances = invoice.performances.map(enrichPerformance);
+        statementData.totalAmount = totalAmount(statementData);
+        statementData.totalVolumeCredits = totalVolumeCredits(statementData);
         return renderPlainText(statementData, plays);
 
         function enrichPerformance(aPerformance) {
@@ -50,6 +52,19 @@ module.exports = {
             if ("comedy" === aPerformance.play.type) {
                 result += Math.floor(aPerformance.audience / 5);
             }
+            return result;
+        }
+        function totalVolumeCredits(data) {
+            let result = 0;
+            for (let perf of data.performances)
+                result += perf.volumeCredits;
+            return result;
+        }
+
+        function totalAmount(data) {
+            let result = 0;
+            for (let perf of data.performances)
+                result += perf.amount;
             return result;
         }
     }
