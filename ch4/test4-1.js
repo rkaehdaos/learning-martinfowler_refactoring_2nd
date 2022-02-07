601acbed30ba0eea1e3f5378e948840be4a66ba3
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-02-07 15:31:41 +0900

    [#4] Province 작성

--- ch4/test4-1.js
+++ ch4/test4-1.js
@@ -12,9 +12,34 @@ function sampleProvinceData() {
 }
 
 class Province {
+    constructor(doc) {
+        this._name = doc.name;
+        this._producers = [];
+        this._totalProduction = 0;
+        this._demand = doc.demand;
+        this._price = doc.price;
+        doc.producers.forEach(value => this.addProducer(new Producer(this, value)));
+    }
 
+    addProducer(arg) {
+        this._producers.push(arg);
+        this._totalProduction += arg.production;
+    }
+
+    //getters
+    get name() {return this._name;}
+    get producers() {return this._producers.slice();} //slice return
+    get totalProduction() {return this._totalProduction;}
+    get demand() {return this._demand;}
+    get price() {return this._price;}
+
+    //setters
+    set totalProduction(value) {this._totalProduction = value;}
+    set demand(value) {this._demand = parseInt(value);}         //숫자로 파싱해서 저장
+    set price(value) {this._price = parseInt(value);}           //숫자로 파싱해서 저장
 }
 
 class Producer {
-
+    constructor(province, value) {
+    }
 }
