9bd6f2f5f5fdeb08e7f23aaf5bbf283b29ed13eb
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-02-07 18:11:16 +0900

    [#4] test 픽스처 중복 제거

--- ch4/test4-1.spec.js
+++ ch4/test4-1.spec.js
@@ -1,16 +1,11 @@
 // import * as chai from "chai";
-import {assert,expect} from "chai";
+import {assert, expect} from "chai";
 import {sampleProvinceData, Province, Producer} from './test4-1.js'
 
 describe('province', () => {
-    it('shortfall', () => {
-        const asia = new Province(sampleProvinceData());
-        expect(asia.shortfall).equal(5);
-    });
-    it('profit', () => {
-        const asia = new Province(sampleProvinceData());
-        expect(asia.profit).equal(230);
-    });
-
+    let asia;
+    beforeEach(() => {asia = new Province(sampleProvinceData());})
+    it('shortfall', () => expect(asia.shortfall).equal(5));
+    it('profit', () => expect(asia.profit).equal(230));
 });
 
