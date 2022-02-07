e7308f611fa6ea2963865a1ee750ef1a97565123
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-02-07 18:06:40 +0900

    [#4] profit 테스트 추가

--- ch4/test4-1.spec.js
+++ ch4/test4-1.spec.js
@@ -1,12 +1,16 @@
-import * as chai from "chai";
+// import * as chai from "chai";
 import {assert,expect} from "chai";
 import {sampleProvinceData, Province, Producer} from './test4-1.js'
 
 describe('province', () => {
     it('shortfall', () => {
         const asia = new Province(sampleProvinceData());
-        assert.equal(asia.shortfall, 5);
         expect(asia.shortfall).equal(5);
     });
+    it('profit', () => {
+        const asia = new Province(sampleProvinceData());
+        expect(asia.profit).equal(230);
+    });
+
 });
 
