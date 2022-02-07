2bdb1faeeb41fcab593edc5579f179acfbf151d2
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-02-07 17:45:15 +0900

    [#4] 줄일수 있도록

--- ch4/test4-1.spec.js
+++ ch4/test4-1.spec.js
@@ -1,10 +1,12 @@
 import * as chai from "chai";
+import {assert,expect} from "chai";
 import {sampleProvinceData, Province, Producer} from './test4-1.js'
 
 describe('province', () => {
     it('shortfall', () => {
         const asia = new Province(sampleProvinceData());
-        chai.assert.equal(asia.shortfall, 5);
+        assert.equal(asia.shortfall, 5);
+        expect(asia.shortfall).equal(5);
     });
 });
 
