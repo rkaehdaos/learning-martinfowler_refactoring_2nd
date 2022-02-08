6500f7e32aceca1f088ca227cc17df26807bed59
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-02-08 09:54:08 +0900

    [#4] 4.5 픽스처 수정하기

--- ch4/test4-1.spec.js
+++ ch4/test4-1.spec.js
@@ -7,5 +7,11 @@ describe('province', () => {
     beforeEach(() => {asia = new Province(sampleProvinceData());})
     it('shortfall', () => expect(asia.shortfall).equal(5));
     it('profit', () => expect(asia.profit).equal(230));
+    it('change production', () => {
+        asia.producers[0].production = 20;
+        expect(asia.shortfall).equal(-6);
+        expect(asia.profit).equal(292);
+    });
 });
+
 
