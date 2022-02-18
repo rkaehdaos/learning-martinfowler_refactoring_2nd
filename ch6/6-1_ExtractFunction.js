ed16edfa9f4538b1494fa35d9d519907b28e73d3
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-02-18 16:04:04 +0900

    [#6 함수 추출하기] 함수 추출하기 적용

--- ch6/6-1_ExtractFunction.js
+++ ch6/6-1_ExtractFunction.js
@@ -9,10 +9,13 @@ function calculateOutstanding() {
 function printOwing(invoice) {
     printBanner();
     let outstanding = calculateOutstanding();
+    printDetails();
 
-    //세부사항 출력
-    console.log(`고객명: ${invoice.customer}`);
-    console.log(`채무액: ${outstanding}`);
+    function printDetails() {
+        //세부사항 출력
+        console.log(`고객명: ${invoice.customer}`);
+        console.log(`채무액: ${outstanding}`);
+    }
 }
 
 export {printOwing};
