ead35e91fb36044bba70497f787030bc67f31f59
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-02-22 16:58:01 +0900

    [#6 함수 추출하기] wrapper 개선 마지막2

--- ch6/Clock.js
+++ ch6/Clock.js
@@ -3,8 +3,10 @@ class Clock {
     constructor(date) { this.date = date; }
 
     static createInstance(arg) {
-        return new Proxy(new this(new Date(arg)), {get: (target, prop) => () => target.date[prop]()});
+        return new Proxy(new this(new Date(arg)),
+            {get: (target, prop) => () => target.date[prop]()});
     }
+
     static today() { return this.createInstance(Date.now()); }
     static day(arg) { return this.createInstance(arg); }
 }
