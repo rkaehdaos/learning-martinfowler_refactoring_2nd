dd97e235eb528c7d6c50d01d835a65a71ef3cbe3
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-02-23 16:16:47 +0900

    [#6 변수 추출하기] quantityDiscount 변수 적용

--- ch6/6-3_extractVariable.js
+++ ch6/6-3_extractVariable.js
@@ -1,7 +1,6 @@
 function price(order) {
     // 가격 = 기본 가격 - 수량 할인 + 배송비
     let basePrice = order.quantity * order.itemPrice;
-    return basePrice -
-        Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-        Math.min(basePrice * 0.1, 100);
+    let quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+    return basePrice - quantityDiscount + Math.min(basePrice * 0.1, 100);
 }
