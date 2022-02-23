ad9f8d32d5eaa4cb7245ae3f1bf5d2ccbca44c25
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-02-23 16:15:52 +0900

    [#6 변수 추출하기] basePrice 적용

--- ch6/6-3_extractVariable.js
+++ ch6/6-3_extractVariable.js
@@ -1,7 +1,7 @@
 function price(order) {
     // 가격 = 기본 가격 - 수량 할인 + 배송비
     let basePrice = order.quantity * order.itemPrice;
-    return order.quantity * order.itemPrice -
+    return basePrice -
         Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-        Math.min(order.quantity * order.itemPrice * 0.1, 100);
+        Math.min(basePrice * 0.1, 100);
 }
