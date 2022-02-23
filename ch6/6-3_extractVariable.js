905c7557aa434c4d0c369298cd696a1f9f27dc14
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-02-23 16:18:34 +0900

    [#6 변수 추출하기] shipping까지 적용 후 주석 삭제
    - 코드에 의도가 드러나므로 주석이 필요 없음

--- ch6/6-3_extractVariable.js
+++ ch6/6-3_extractVariable.js
@@ -1,6 +1,6 @@
 function price(order) {
-    // 가격 = 기본 가격 - 수량 할인 + 배송비
-    let basePrice = order.quantity * order.itemPrice;
-    let quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
-    return basePrice - quantityDiscount + Math.min(basePrice * 0.1, 100);
+    const basePrice = order.quantity * order.itemPrice;
+    const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+    const shipping = Math.min(basePrice * 0.1, 100);
+    return basePrice - quantityDiscount + shipping;
 }
