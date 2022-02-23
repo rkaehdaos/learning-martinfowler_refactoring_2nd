1d7998a2853058844e1708739917c780f35dad85
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-02-23 16:14:50 +0900

    [#6 변수 추출하기] 변수 선언

--- ch6/6-3_extractVariable.js
+++ ch6/6-3_extractVariable.js
@@ -1,5 +1,6 @@
 function price(order) {
     // 가격 = 기본 가격 - 수량 할인 + 배송비
+    let basePrice = order.quantity * order.itemPrice;
     return order.quantity * order.itemPrice -
         Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
         Math.min(order.quantity * order.itemPrice * 0.1, 100);
