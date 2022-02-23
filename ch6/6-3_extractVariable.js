0ed19bdb65cb8f81d5856e123e7b8c424e23eec4
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-02-23 16:23:09 +0900

    [#6 변수 추출하기] ex2

--- ch6/6-3_extractVariable.js
+++ ch6/6-3_extractVariable.js
@@ -1,6 +1,23 @@
+//ex1
+/*
 function price(order) {
     const basePrice = order.quantity * order.itemPrice;
     const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
     const shipping = Math.min(basePrice * 0.1, 100);
     return basePrice - quantityDiscount + shipping;
+}
+*/
+
+//ex2
+class Order{
+    constructor(aRecord) {
+        this._data = aRecord;
+    }
+    get quantity() {return this._data.quantity;}
+    get itemPrice() {return this._data.itemPrice;}
+    get price() {
+        return order.quantity * order.itemPrice -
+            Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+            Math.min(order.quantity * order.itemPrice * 0.1, 100);
+    }
 }
