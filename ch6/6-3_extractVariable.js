99827dbdf26d7b030fc366c33bd01f5a342b69bc
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-02-23 16:30:12 +0900

    [#6 변수 추출하기] 표현식을 함수로 추출

--- ch6/6-3_extractVariable.js
+++ ch6/6-3_extractVariable.js
@@ -16,8 +16,10 @@ class Order{
     get quantity() {return this._data.quantity;}
     get itemPrice() {return this._data.itemPrice;}
     get price() {
-        return order.quantity * order.itemPrice -
-            Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-            Math.min(order.quantity * order.itemPrice * 0.1, 100);
+        return this.basePrice - this.quantityDisCount +
+            this.shipping;
     }
+    get basePrice() {return order.quantity * order.itemPrice; }
+    get quantityDisCount() {return Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;}
+    get shipping() {return Math.min(this.basePrice() * 0.1, 100);}
 }
