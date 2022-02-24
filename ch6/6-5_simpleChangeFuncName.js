b180d393afe0f522dc3d96299a4e48d4d02f69aa
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-02-24 16:55:11 +0900

    #20 PR : 6-5 change Func Delete
    * [#6 함수이름 간단히 바꾸기] 함수 이름 축약된 예제
    
    * [#6 함수이름 간단히 바꾸기] 함수 이름 축약된 예제
    
    * [#6 함수이름 간단히 바꾸기] 함수 선언 수정
    
    * [#6 함수이름 간단히 바꾸기] 마이그레이션 절차 따른 예제
    
    * [#6 함수이름 바꾸기(mig)] 본문 전체를 새로운 함수로 추출
    
    * [#6 함수이름 바꾸기(mig)] 원래 함수가 새로운 함수를 호출하는 전달 함수 역할 하도록
    
    * [#6 함수이름 바꾸기(mig)] 예전 함수 호출 부분을 모두 새함수를 호출하도록 바꾼다
    
    * [#6 함수이름 바꾸기(mig)] 기존 함수 삭제

---
+++ ch6/6-5_simpleChangeFuncName.js
@@ -0,0 +1,9 @@
+function circumference(radius) {
+    return 2 * Math.PI * radius;
+}
+
+function mig_circumference(radius) {
+    return 2 * Math.PI * radius;
+}
+
+export {circumference, mig_circumference};
---
+++ ch6/6-5_simpleChangeFuncName.spec.js
@@ -0,0 +1,14 @@
+import {expect} from "chai";
+import {circumference, mig_circumference} from "./6-5_simpleChangeFuncName.js";
+
+describe("함수 선언 바꾸기", () => {
+    it('함수 이름 바꾸기', () => {
+        expect(circumference(1)).equal(2 * Math.PI);
+        expect(circumference(50)).equal(100 * Math.PI);
+    });
+    it('이름 바꾸기 (마이그레이션', () => {
+        expect(mig_circumference(1)).equal(2 * Math.PI);
+        expect(mig_circumference(50)).equal(100 * Math.PI);
+    });
+
+})
