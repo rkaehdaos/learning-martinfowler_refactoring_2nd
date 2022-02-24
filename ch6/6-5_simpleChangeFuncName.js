c78fd491f439141bce25b99b34842b8a676b2b4f
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-02-24 15:20:15 +0900

    [#6 함수이름 바꾸기(mig)] 기존 함수 삭제

--- ch6/6-5_simpleChangeFuncName.js
+++ ch6/6-5_simpleChangeFuncName.js
@@ -2,10 +2,6 @@ function circumference(radius) {
     return 2 * Math.PI * radius;
 }
 
-function mig_circum(radius) {
-    return mig_circumference(radius);
-}
-
 function mig_circumference(radius) {
     return 2 * Math.PI * radius;
 }
