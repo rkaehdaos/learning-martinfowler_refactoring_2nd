1629eced46064416e6b1b1fb082d6060633783a9
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-02-28 11:11:18 +0900

    [#6 매개변수 객체] 1.먼저 묶은 데이터를 표현하는 클래스 선언

--- ch6/6-8_introduceParameterObject.js
+++ ch6/6-8_introduceParameterObject.js
@@ -3,4 +3,13 @@ function readingsOutsideRange(station, min, max) {
     return station.readings.filter((r) => r.temp < min || r.temp > max)
 }
 
+class NumberRange {
+    constructor(min, max) {
+        this._data = {min: min, max: max};
+    }
+    get min() {return this._data.min;}
+    get max() {return this._data.max;}
+}
+
+
 export {readingsOutsideRange};
