a056058535c7a30b318976c2efb13242798b285f
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-02-28 15:45:29 +0900

    [#6 단계쪼개기] 1. 뒷부분 배송비 계산 부분을 함수로 추출 : 뒷부분 확인

--- ch6/6-11_splitPhase.js
+++ ch6/6-11_splitPhase.js
@@ -2,6 +2,9 @@ function priceOrder(product, quantity, shippingMethod) {
     const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0)
         * product.basePrice * product.discountRate;
+
+
+
     const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
         ? shippingMethod.discountedFee : shippingMethod.feePerCase;
     const shippingCost = quantity * shippingPerCase;
