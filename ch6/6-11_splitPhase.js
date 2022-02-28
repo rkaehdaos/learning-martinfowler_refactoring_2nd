03c6177ed8fa1495987efcd1e4338022fdca2ae6
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-02-28 16:15:49 +0900

    [#6 단계쪼개기] 6. 1step 코드 함수 추출하면서 중간 데이터 구조를 반환 - 5,6
    - 원본 함수에서 추출한 코드 부분을 새로 만든 함수 호출 문장으로 바꾼다(위임)
    - 테스트

--- ch6/6-11_splitPhase.js
+++ ch6/6-11_splitPhase.js
@@ -1,12 +1,5 @@
 function priceOrder(product, quantity, shippingMethod) {
-    const basePrice = product.basePrice * quantity;
-    const discount = Math.max(quantity - product.discountThreshold, 0)
-        * product.basePrice * product.discountRate;
-    const priceData = {
-        basePrice: basePrice,
-        quantity: quantity,
-        discount: discount
-    };
+    const priceData = calculatePricingData(product, quantity);
     const price = applyShipping(priceData, shippingMethod);
     return price;
 }
@@ -20,7 +13,7 @@ function calculatePricingData(product, quantity) {
         quantity: quantity,
         discount: discount
     };
-    
+    return priceData;
 }
 
 function applyShipping(priceData, shippingMethod) {
