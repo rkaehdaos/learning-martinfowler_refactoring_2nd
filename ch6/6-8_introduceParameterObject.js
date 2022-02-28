c3abf71e5880eaf79c467ca4d9cddd25a768429f
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-02-28 11:19:41 +0900

    [#6 매개변수 객체] 4. 기존매개변수 변경 - 최소값

--- ch6/6-8_introduceParameterObject.js
+++ ch6/6-8_introduceParameterObject.js
@@ -1,6 +1,6 @@
 // 정상 범위를 벗어난 측정값을 찾는 함수
-function readingsOutsideRange(station, min, range) {
-    return station.readings.filter((r) => r.temp < min || r.temp > range.max)
+function readingsOutsideRange(station, range) {
+    return station.readings.filter((r) => r.temp < range.min || r.temp > range.max);
 }
 
 class NumberRange {
