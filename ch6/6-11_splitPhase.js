5eb8d800bcaf26579a9d63d59c0693a2a0e1d208
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-02-28 16:17:13 +0900

    [#6 단계쪼개기] 정리 - 최종결과 상수 정리

--- ch6/6-11_splitPhase.js
+++ ch6/6-11_splitPhase.js
@@ -1,19 +1,17 @@
 function priceOrder(product, quantity, shippingMethod) {
     const priceData = calculatePricingData(product, quantity);
-    const price = applyShipping(priceData, shippingMethod);
-    return price;
+    return applyShipping(priceData, shippingMethod);
 }
 
 function calculatePricingData(product, quantity) {
     const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0)
         * product.basePrice * product.discountRate;
-    const priceData = {
+    return {
         basePrice: basePrice,
         quantity: quantity,
         discount: discount
     };
-    return priceData;
 }
 
 function applyShipping(priceData, shippingMethod) {
