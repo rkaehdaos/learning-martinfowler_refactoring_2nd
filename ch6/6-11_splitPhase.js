67fb4318d1142a40e1d670f362e8c7bbca5a2e34
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-02-28 16:06:39 +0900

    [#6 단계쪼개기] 5. 2step 함수 매개변수 하나씩 검토해서 1step에서 사용되는것은 중간 데이터구조로 : discount

--- ch6/6-11_splitPhase.js
+++ ch6/6-11_splitPhase.js
@@ -4,17 +4,18 @@ function priceOrder(product, quantity, shippingMethod) {
         * product.basePrice * product.discountRate;
     const priceData = {
         basePrice: basePrice,
-        quantity: quantity
+        quantity: quantity,
+        discount: discount
     };
-    const price = applyShipping(priceData, shippingMethod,  discount);
+    const price = applyShipping(priceData, shippingMethod);
     return price;
 }
 
-function applyShipping(priceData, shippingMethod,  discount) {
+function applyShipping(priceData, shippingMethod) {
     const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
         ? shippingMethod.discountedFee : shippingMethod.feePerCase;
     const shippingCost = priceData.quantity * shippingPerCase;
-    const price = priceData.basePrice - discount + shippingCost;
+    const price = priceData.basePrice - priceData.discount + shippingCost;
     return price;
 }
 
