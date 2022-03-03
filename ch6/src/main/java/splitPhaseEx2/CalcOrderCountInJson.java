cc87d42c512c370c4ccfa349e298c893c8de6b88
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-03 10:58:57 +0900

    [#6 단계쪼개기ex2] Sysout을 호출한 곳으로 옮기기

--- ch6/src/main/java/splitPhaseEx2/CalcOrderCountInJson.java
+++ ch6/src/main/java/splitPhaseEx2/CalcOrderCountInJson.java
@@ -10,14 +10,14 @@ import java.util.stream.Stream;
 public class CalcOrderCountInJson {
     public static void main(String[] args) {
         try {
-            run(args);
+            System.out.println(run(args));;
         } catch (Exception e) {
             System.err.println(e);
             System.exit(1);
         }
     }
 
-    static void run(String[] args) throws IOException{
+    static long run(String[] args) throws IOException{
         if(args.length==0) throw new RuntimeException("파일명을 입력하세요");
         String filename = args[args.length - 1];
         File inputFile = CalcOrderCountInJson.getFile(filename);
@@ -25,12 +25,11 @@ public class CalcOrderCountInJson {
         ObjectMapper mapper = new ObjectMapper();
         Order[] orders = mapper.readValue(inputFile, Order[].class);
         if (Stream.of(args).anyMatch(arg -> "-r".equals(arg)))
-            System.out.println(Stream.of(orders)
+            return Stream.of(orders)
                     .filter(o -> "ready".equals(o.getStatus()))
-                    .count());
+                    .count();
         else
-            System.out.println(orders.length);
-
+            return orders.length;
     }
 
 
