cce1dcc19203db55a06d446036ad430bfa07f316
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-03 15:54:35 +0900

    [#7 레코드캡슐화ex1] 마무리 - 깔끔하게 필드 flating

--- ch7/7-1_encapsulateRecord.js
+++ ch7/7-1_encapsulateRecord.js
@@ -1,9 +1,12 @@
-class Organization{
-    constructor(data) { this._data = data; }
-    set name(aString) { this._data.name = aString; }
-    get name() { return this._data.name; }
-    set country(aString) { this._data.country = aString; }
-    get country() { return this._data.country; }
+class Organization {
+    constructor(data) {
+        this._name = data.name;
+        this._country = data.country;
+    }
+    set name(aString) { this._name = aString; }
+    get name() { return this._name; }
+    set country(aString) { this._country = aString; }
+    get country() { return this._country; }
 }
 
 const organization = new Organization({name: "애크미 구스베리", country: "GB"});
