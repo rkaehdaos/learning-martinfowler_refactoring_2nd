f7f34c2fd749ee8fb0971ce6f5857126ccfc95f1
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-03 15:49:37 +0900

    [#7 레코드캡슐화ex1] 2.레코드 래퍼 클래스로 교체5-getter
    - 필드 접근시 객체 접근자 사용
    - 적절한 접근자가 없다면 추가한다
    - 레코드 읽는 코드는 모두 getter를 사용하도록

--- ch7/7-1_encapsulateRecord.js
+++ ch7/7-1_encapsulateRecord.js
@@ -1,6 +1,7 @@
 class Organization{
     constructor(data) { this._data = data; }
     set name(aString) { this._data.name = aString; }
+    get name() { return this._data.name; }
 }
 
 const organization = new Organization({name: "애크미 구스베리", country: "GB"});
--- ch7/7-1_encapsulateRecord.spec.js
+++ ch7/7-1_encapsulateRecord.spec.js
@@ -3,9 +3,9 @@ import {getRawDataOfOrganization,getOrganization} from "./7-1_encapsulateRecord.
 
 describe("7-1 레코드 캡슐화", () => {
     it("변수 캡슐화 테스트", () => {
-        expect(getRawDataOfOrganization().name).to.equal('애크미 구스베리');
+        expect(getOrganization().name).to.equal('애크미 구스베리');
         getOrganization().name = 'new name';
-        expect(getRawDataOfOrganization().name).to.equal('new name');
+        expect(getOrganization().name).to.equal('new name');
         expect(getRawDataOfOrganization().country).to.equal('GB');
     });
 });
