67ff812833dbb0af8e47c1592c632f9d8cb34e1d
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-03 11:20:55 +0900

    [#6 단계쪼개기ex2] 절차 3,4
    - 중간데이터 구조를 만들어 앞에서 추춝한 함수 인수로 추가
    - 테스트

--- ch6/src/main/java/splitPhaseEx2/CalcOrderCountInJson.java
+++ ch6/src/main/java/splitPhaseEx2/CalcOrderCountInJson.java
@@ -10,20 +10,21 @@ import java.util.stream.Stream;
 public class CalcOrderCountInJson {
     public static void main(String[] args) {
         try {
-            System.out.println(run(args));;
+            System.out.println(run(args));
         } catch (Exception e) {
             System.err.println(e);
             System.exit(1);
         }
     }
 
-    static long run(String[] args) throws IOException{
-        if(args.length==0) throw new RuntimeException("파일명을 입력하세요");
+    static long run(String[] args) throws IOException {
+        if (args.length == 0) throw new RuntimeException("파일명을 입력하세요");
+        CommandLine commandLine = new CommandLine();
         String filename = args[args.length - 1];
-        return countOrders(args, filename);
+        return countOrders(commandLine, args, filename);
     }
 
-    private static long countOrders(String[] args, String filename) throws IOException{
+    private static long countOrders(CommandLine commandLine, String[] args, String filename) throws IOException {
         File inputFile = CalcOrderCountInJson.getFile(filename);
         ObjectMapper mapper = new ObjectMapper();
         Order[] orders = mapper.readValue(inputFile, Order[].class);
@@ -42,4 +43,6 @@ public class CalcOrderCountInJson {
             return new File(resource.getFile());
         }
     }
+
+    private static class CommandLine {}
 }
