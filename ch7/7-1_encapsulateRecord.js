ca72ec0e7fe662b62f4e1381a1491d37f0fea66f
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-03 15:41:08 +0900

    [#7 레코드캡슐화ex1] 2.레코드 래퍼 클래스로 교체1
    - 레코드를 클래스로 바꾼다

--- ch7/7-1_encapsulateRecord.js
+++ ch7/7-1_encapsulateRecord.js
@@ -1,4 +1,12 @@
-const organization = {name: "애크미 구스베리", country: "GB"};
-function getRawDataOfOrganization() {return organization;}
+class Organization{
+    constructor(data) {
+        this._data = data;
+    }
+}
+
+const organization = new Organization({name: "애크미 구스베리", country: "GB"});
+function getRawDataOfOrganization() {return organization._data;}
+
+
 
 export {organization,getRawDataOfOrganization};
--- ch7/7-1_encapsulateRecord.spec.js
+++ ch7/7-1_encapsulateRecord.spec.js
@@ -1,15 +1,9 @@
 import {expect} from "chai";
-import {getRawDataOfOrganization, organization} from "./7-1_encapsulateRecord.js";
+import {getRawDataOfOrganization} from "./7-1_encapsulateRecord.js";
 
 describe("7-1 레코드 캡슐화", () => {
-    it("기본", () => {
-        const result = organization;
-        expect(result).to.have.property('name').that.is.equal('애크미 구스베리');
-        expect(result).to.have.property('country').that.is.equal('GB');
-    });
     it("변수 캡슐화 테스트", () => {
-        expect(getRawDataOfOrganization()).to.have.property('name').that.is.equal('애크미 구스베리');
-        expect(getRawDataOfOrganization()).to.have.property('country').that.is.equal('GB');
+
         expect(getRawDataOfOrganization().name).to.equal('애크미 구스베리');
         expect(getRawDataOfOrganization().country).to.equal('GB');
     });
