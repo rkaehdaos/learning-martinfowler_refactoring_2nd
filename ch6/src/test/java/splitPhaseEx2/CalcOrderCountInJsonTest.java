81b6d50107ed3b927c4e0e87b27f2126ac89c94a
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-03 10:50:25 +0900

    [#6 단계쪼개기ex2] 테스트 수정,

--- ch6/src/test/java/splitPhaseEx2/CalcOrderCountInJsonTest.java
+++ ch6/src/test/java/splitPhaseEx2/CalcOrderCountInJsonTest.java
@@ -1,6 +1,7 @@
 package splitPhaseEx2;
 
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
@@ -20,12 +21,26 @@ class CalcOrderCountInJsonTest {
     }
 
     @Test
+    @DisplayName("r이 없으면 전체 개수 5개")
     void test1() throws Exception {
         //given
         CalcOrderCountInJson.main(new String[]{"testData.json"});
         
         //when
         
+        //then
+    }
+
+    @Test
+    @DisplayName("r이 있으면 ready 3개")
+    void test2() throws Exception {
+        //given
+
+        String[] args = {"-r", "testData.json"};
+        CalcOrderCountInJson.main(args);
+
+        //when
+
         //then
     }
 
