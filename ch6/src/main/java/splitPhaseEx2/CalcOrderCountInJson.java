87be34d5f61364e8a97bc2ab65b5f8c3f700bfa1
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-03 10:54:59 +0900

    [#6 단계쪼개기ex2] 핵식 작업 코드 전부 함수로 추출5
    - 위임후 테스트 - 성공

--- ch6/src/main/java/splitPhaseEx2/CalcOrderCountInJson.java
+++ ch6/src/main/java/splitPhaseEx2/CalcOrderCountInJson.java
@@ -10,20 +10,7 @@ import java.util.stream.Stream;
 public class CalcOrderCountInJson {
     public static void main(String[] args) {
         try {
-
-            if(args.length==0) throw new RuntimeException("파일명을 입력하세요");
-            String filename = args[args.length - 1];
-            File inputFile = CalcOrderCountInJson.getFile(filename);
-
-            ObjectMapper mapper = new ObjectMapper();
-            Order[] orders = mapper.readValue(inputFile, Order[].class);
-            if (Stream.of(args).anyMatch(arg -> "-r".equals(arg)))
-                System.out.println(Stream.of(orders)
-                        .filter(o -> "ready".equals(o.getStatus()))
-                        .count());
-            else
-                System.out.println(orders.length);
-
+            run(args);
         } catch (Exception e) {
             System.err.println(e);
             System.exit(1);
