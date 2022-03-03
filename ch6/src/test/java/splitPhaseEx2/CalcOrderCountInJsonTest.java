c4cf11c93c4bcd0adb6680a51cf8a08e4626a142
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-03 11:04:00 +0900

    [#6 단계쪼개기ex2] 제대로된 JUnit 테스트 성공

--- ch6/src/test/java/splitPhaseEx2/CalcOrderCountInJsonTest.java
+++ ch6/src/test/java/splitPhaseEx2/CalcOrderCountInJsonTest.java
@@ -1,47 +1,23 @@
 package splitPhaseEx2;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-
-
-import static org.assertj.core.api.Assertions.*;
-
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CalcOrderCountInJsonTest {
 
-    @Test
-    @Disabled
-    void errrorTest() throws Exception {
-
-        assertThatThrownBy(() -> CalcOrderCountInJson.main(new String[]{}))
-                ;
-
-    }
 
     @Test
     @DisplayName("r이 없으면 전체 개수 5개")
     void test1() throws Exception {
-        //given
-        CalcOrderCountInJson.main(new String[]{"testData.json"});
-        
-        //when
-        
-        //then
+        assertThat(CalcOrderCountInJson.run(new String[]{"testData.json"})).isEqualTo(5);
     }
 
     @Test
     @DisplayName("r이 있으면 ready 3개")
     void test2() throws Exception {
-        //given
-
-        String[] args = {"-r", "testData.json"};
-        CalcOrderCountInJson.main(args);
-
-        //when
-
-        //then
+        assertThat(CalcOrderCountInJson.run(new String[]{"-r", "testData.json"})).isEqualTo(3);
     }
 
 }
