6aac86f7bdce6e9140d1a4f46ae8d87492f4275b
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-03 15:51:09 +0900

    [#7 레코드캡슐화ex1] 2.레코드 래퍼 클래스로 교체5-country도

--- ch7/7-1_encapsulateRecord.js
+++ ch7/7-1_encapsulateRecord.js
@@ -2,6 +2,8 @@ class Organization{
     constructor(data) { this._data = data; }
     set name(aString) { this._data.name = aString; }
     get name() { return this._data.name; }
+    set country(aString) { this._data.country = aString; }
+    get country() { return this._data.country; }
 }
 
 const organization = new Organization({name: "애크미 구스베리", country: "GB"});
