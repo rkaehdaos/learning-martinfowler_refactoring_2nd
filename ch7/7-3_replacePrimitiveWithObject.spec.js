9fa0383430274a3bd655c3c9eb612568992f32bd
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-04 17:13:57 +0900

    7-3_기본형객체교체 PR Merge #26
    * [#7 기본형객체교체ex] 단순한 주문 클래스 예시
    
    * [#7 기본형객체교체ex] 1. 변수부터 캡슐화
    
    * [#7 기본형객체교체ex] 1-1. Field Self-encapsulation
    
    * [#7 기본형객체교체ex] 2. Value Class - Priority
    
    * [#7 기본형객체교체ex] 4,5 : 방금 만든 값 객체 이용하도록 get/set 수정
    
    * [#7 기본형객체교체ex] 7. 함수 이름 바꾸기
    
    * [#7 기본형객체교체ex더가다듬기] 1. priority객체 getter 만들기
    
    * [#7 기본형객체교체ex더가다듬기] 1. priority객체 getter 만들기 오타수정
    
    * [#7 기본형객체교체ex더가다듬기] 2. Order Setter가 Priority 인스턴스 받도록 수정
    
    * [#7 기본형객체교체ex더가다듬기] 오타 수정
    
    * [#7 기본형객체교체ex더가다듬기] 3. 새로운 동작 : 우선순위 검증, 비교 로직 추가
    
    * [#7 기본형객체교체ex더가다듬기] 3-1. 새로운 동작 테스트

---
+++ ch7/7-3_replacePrimitiveWithObject.spec.js
@@ -0,0 +1,34 @@
+import {expect} from "chai";
+import {Order,Priority} from "./7-3_replacePrimitiveWithObject.js";
+describe('7-3기본형객체형바꾸기',() => {
+    it('higherThan Test', () => {
+        //given
+        let orders = [];
+        let orderList = ["high", "high", "rush", "low", "low"];
+
+        //when
+        orderList.map((value, index) => orders.push(new Order({"priority": value, "orderNum":index})));
+        //then
+        expect(orders).to.length(5);
+
+        //when
+        const highPriorityCount = orders.filter(o => o.priority.higherThan(new Priority("normal")) ).length;
+        //then
+        expect(highPriorityCount).equals(3);
+    });
+    it('바뀐 settter 테스트', () => {
+        //given
+        let orders = [];
+        let orderList = ["high", "high", "rush", "low", "low"];
+
+        //when
+        orderList.map((value, index) => orders.push(new Order({"priority": new Priority(value), "orderNum":index})));
+        //then
+        expect(orders).to.length(5);
+
+        //when
+        const highPriorityCount = orders.filter(o => "high" === o.priorityString || "rush"===o.priorityString ).length;
+        //then
+        expect(highPriorityCount).equals(3);
+    });
+})
