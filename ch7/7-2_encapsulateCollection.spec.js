40e602db21d7b3a76e2a0d25246fa7f75ef1d351
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-04 12:17:01 +0900

    [#7 컬렉션캡슐화ex] 클라이언트 추가 : 잘못된 방식 사용하는

--- ch7/7-2_encapsulateCollection.spec.js
+++ ch7/7-2_encapsulateCollection.spec.js
@@ -6,4 +6,17 @@ describe('7-2컬렉션캡슐화',() => {
         const numCourses = aPerson.courses.length;
         expect(numCourses).equals(0);
     });
+    it('클라이언트테스트', () => {
+        //given
+        const aPerson = new Person('Ahn');
+        const aCoursesNames = ["korean", "math", "english", "social","science"];
+
+        //when
+        aPerson.courses = aCoursesNames.map(name => new Course(name,false));
+        console.log(aPerson.courses);
+
+        const numCourses = aPerson.courses.length;
+        expect(numCourses).equals(5);
+    });
+
 } )
