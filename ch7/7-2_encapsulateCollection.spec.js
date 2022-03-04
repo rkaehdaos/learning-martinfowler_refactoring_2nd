d6960361253a7efae1e1207c027fc00edb73a48c
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-04 12:21:50 +0900

    [#7 컬렉션캡슐화ex] 클라이언트 추가2 - 잘못된 방식 2
    - 클라이언트에서는 이렇게 직접 수정이 편하게 느껴지므로

--- ch7/7-2_encapsulateCollection.spec.js
+++ ch7/7-2_encapsulateCollection.spec.js
@@ -1,22 +1,37 @@
 import {expect} from "chai";
-import {Person,Course} from "./7-2_encapsulateCollection.js";
-describe('7-2컬렉션캡슐화',() => {
+import {Person, Course} from "./7-2_encapsulateCollection.js";
+
+describe('7-2컬렉션캡슐화', () => {
     it('기본테스트', () => {
         const aPerson = new Person('Ahn');
         const numCourses = aPerson.courses.length;
         expect(numCourses).equals(0);
     });
-    it('클라이언트테스트', () => {
+    it('클라이언트테스트1', () => {
         //given
         const aPerson = new Person('Ahn');
-        const aCoursesNames = ["korean", "math", "english", "social","science"];
+        const aCoursesNames = ["korean", "math", "english", "social", "science"];
 
         //when
-        aPerson.courses = aCoursesNames.map(name => new Course(name,false));
+        aPerson.courses = aCoursesNames.map(name => new Course(name, false));
+        console.log(aPerson.courses);
+
+        const numCourses = aPerson.courses.length;
+        expect(numCourses).equals(5);
+    });
+    it('클라이언트테스트2', () => {
+        //given
+        const aPerson = new Person('Ahn');
+        const aCoursesNames = ["korean", "math", "english", "social", "science"];
+
+        //when
+        for(const name of aCoursesNames)
+            aPerson.courses.push(new Course(name,false));
+
         console.log(aPerson.courses);
 
         const numCourses = aPerson.courses.length;
         expect(numCourses).equals(5);
     });
 
-} )
+})
