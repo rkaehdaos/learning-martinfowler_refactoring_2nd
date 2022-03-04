764000d1300118480fd28cba09cde2316cfe45a3
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-04 17:11:16 +0900

    [#7 기본형객체교체ex더가다듬기] 3. 새로운 동작 : 우선순위 검증, 비교 로직 추가

--- ch7/7-3_replacePrimitiveWithObject.js
+++ ch7/7-3_replacePrimitiveWithObject.js
@@ -1,9 +1,18 @@
 class Priority {
     constructor(value) {
         if (value instanceof Priority) return value;
-        this._value = value;
+        if (Priority.legalValues().includes(value))
+            this._value = value;
+        else
+            throw new Error(`<${value}>는 유효하지 않은 우선순위입니다.`);
     }
     toString() {return this._value;}
+    get _index() {return Priority.legalValues().findIndex(s => s === this._value);}
+    static legalValues() {return ['low', 'normal', 'high', 'rush'];}
+    equals(other) {return this._index === other._index;}
+    higherThan(other) {return this._index > other._index;}
+    lowerThan(other) {return this._index < other._index;}
+
 }
 
 class Order {
