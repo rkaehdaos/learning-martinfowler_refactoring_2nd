23c2b458c43031d9184e467cb4547804517d0890
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-04 15:51:00 +0900

    [#7 기본형객체교체ex] 4,5 : 방금 만든 값 객체 이용하도록 get/set 수정

--- ch7/7-3_replacePrimitiveWithObject.js
+++ ch7/7-3_replacePrimitiveWithObject.js
@@ -9,8 +9,8 @@ class Order {
         this.orderNum = data.orderNum;
     }
 
-    get priority() {return this._priority;}
-    set priority(aString) {this._priority = aString;}
+    get priority() {return this._priority.toString();}
+    set priority(aString) {this._priority = new Priority(aString);}
     get orderNum() {return this._orderNum;}
     set orderNum(value) {this._orderNum = value;}
 }
