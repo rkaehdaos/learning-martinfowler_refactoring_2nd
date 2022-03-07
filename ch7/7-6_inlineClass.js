e2173009dea02518f2356174e0d3eb2dcd6e842d
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-07 14:09:24 +0900

    [#7 클래스인라인 ex] 4. 클래스 삭제

--- ch7/7-6_inlineClass.js
+++ ch7/7-6_inlineClass.js
@@ -1,22 +1,9 @@
-class TrackingInformation {
-    get shippingCompany() {return this._shippingCompany;} //배송 회사
-    set shippingCompany(arg) {this._shippingCompany=arg;}
-    get trackingNumber() {return this._trackingNumber;}
-    set trackingNumber(arg) {this._trackingNumber=arg;}
-    get display() {
-        return `${this.shippingCompany}: ${this.trackingNumber}`;
-    }
-}
 class Shipment {
     get trackingNumber() {return this._trackingNumber;}
     set trackingNumber(arg) {this._trackingNumber=arg;}
     get shippingCompany() {return this._shippingCompany;}
     set shippingCompany(arg) {this._shippingCompany=arg}
     get trackingInfo(){return `${this.shippingCompany}: ${this.trackingNumber}`;}
-    get trackingInformation() {return this._trackingInformation;}
-    set trackingInformation(aTrackingInformation) {
-        this._trackingInformation=aTrackingInformation;
-    }
 }
 
-export {TrackingInformation, Shipment};
+export { Shipment};
--- ch7/7-6_inlineClass.spec.js
+++ ch7/7-6_inlineClass.spec.js
@@ -1,10 +1,9 @@
 import {expect} from "chai";
-import {Shipment, TrackingInformation} from "./7-6_inlineClass.js";
+import {Shipment} from "./7-6_inlineClass.js";
 
 describe('7-6 클래스 인라인화', () => {
     it('default', () => {
         const aShipment = new Shipment();
-        aShipment.trackingInformation = new TrackingInformation();
         aShipment.shippingCompany= 'CJ택배';
         aShipment.trackingNumber = '12345678';
         expect(aShipment.trackingInfo).equals('CJ택배: 12345678');
