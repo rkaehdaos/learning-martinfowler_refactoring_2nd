6cdda40578300791562167e2f5b6fb70e679b338
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-07 14:04:53 +0900

    [#7 클래스인라인 ex] hotfix - 오류

--- ch7/7-6_inlineClass.js
+++ ch7/7-6_inlineClass.js
@@ -4,7 +4,7 @@ class TrackingInformation {
     get trackingNumber() {return this._trackingNumber;}
     set trackingNumber(arg) {this._trackingNumber=arg;}
     get display() {
-        return `${this.shippingCompany}: ${this._trackingNumber}`;
+        return `${this.shippingCompany}: ${this.trackingNumber}`;
     }
 }
 class Shipment {
