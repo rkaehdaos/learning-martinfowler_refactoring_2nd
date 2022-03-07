b1d37673f08050a4aea350709dad2023ad9c7f2b
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-07 14:06:36 +0900

    [#7 클래스인라인 ex] 3. 소스→타깃 : 필드2

--- ch7/7-6_inlineClass.js
+++ ch7/7-6_inlineClass.js
@@ -8,8 +8,8 @@ class TrackingInformation {
     }
 }
 class Shipment {
-    get trackingNumber() {return this._trackingInformation.trackingNumber;}
-    set trackingNumber(arg) {this._trackingInformation.trackingNumber=arg;}
+    get trackingNumber() {return this._trackingNumber;}
+    set trackingNumber(arg) {this._trackingNumber=arg;}
     get shippingCompany() {return this._shippingCompany;}
     set shippingCompany(arg) {this._shippingCompany=arg}
     get trackingInfo(){return `${this.shippingCompany}: ${this.trackingNumber}`;}
