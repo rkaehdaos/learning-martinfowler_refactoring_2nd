63bed2b7429dd6796464a0bf90c70310a8074a72
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-07 11:52:40 +0900

    [#7 클래스추출 ex] 6. 정리1:
    - 새 클래스는 순수 전화번호, office사용 이유없음
    - 전화번호라는 뜻도 메서드 이름에서 다시 강조할 필요가 없음
    - 메서드들의 이름을 적절히 바꿔주자

--- ch7/7-5_extractClass.js
+++ ch7/7-5_extractClass.js
@@ -1,9 +1,9 @@
 class TelephoneNumber {
-    get officeAreaCode() {return this._officeAreaCode;}
-    set officeAreaCode(arg) {this._officeAreaCode=arg;}
-    get officeNumber() {return this._officeNumber}
-    set officeNumber(arg) {this._officeNumber=arg;}
-    get telephoneNumber() {return `(${this.officeAreaCode}) ${this.officeNumber}`;}
+    get areaCode() {return this._areaCode;}
+    set areaCode(arg) {this._areaCode=arg;}
+    get number() {return this._number}
+    set number(arg) {this._number=arg;}
+    get telephoneNumber() {return `(${this.areaCode}) ${this.number}`;}
 }
 class Person {
     constructor() {
@@ -12,10 +12,10 @@ class Person {
     get name() {return this._name;}
     set name(arg) {this._name=arg;}
     get telephoneNumber() {return this._telephoneNumber.telephoneNumber;}
-    get officeAreaCode() {return this._telephoneNumber.officeAreaCode;}
-    set officeAreaCode(arg) {this._telephoneNumber.officeAreaCode=arg;}
-    get officeNumber() {return this._telephoneNumber.officeNumber}
-    set officeNumber(arg) {this._telephoneNumber.officeNumber=arg;}
+    get areaCode() {return this._telephoneNumber.areaCode;}
+    set areaCode(arg) {this._telephoneNumber.areaCode=arg;}
+    get number() {return this._telephoneNumber.number}
+    set number(arg) {this._telephoneNumber.number=arg;}
 }
 
 export {Person};
