509952aa37e39ca06dafd84369526e6fef8d4881
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-07 12:26:03 +0900

    [#7 클래스인라인 ex] 0.클라이언트가 현재 shippment일부처럼 사용중

--- ch7/7-6_inlineClass.spec.js
+++ ch7/7-6_inlineClass.spec.js
@@ -1,15 +1,12 @@
 import {expect} from "chai";
-import {TrackingInformation,Shipment} from "./7-6_inlineClass.js";
+import {Shipment, TrackingInformation} from "./7-6_inlineClass.js";
 
 describe('7-6 클래스 인라인화', () => {
     it('default', () => {
-        const aTrackingInformation = new TrackingInformation();
-        aTrackingInformation.shippingCompany = 'CJ택배';
-        aTrackingInformation.trackingNumber = '12345678';
-
         const aShipment = new Shipment();
-        aShipment.trackingInformation = aTrackingInformation;
-
+        aShipment.trackingInformation = new TrackingInformation();
+        aShipment.trackingInformation.shippingCompany= 'CJ택배';
+        aShipment.trackingInformation.trackingNumber = '12345678';
         expect(aShipment.trackingInfo).equals('CJ택배: 12345678');
     });
 });
