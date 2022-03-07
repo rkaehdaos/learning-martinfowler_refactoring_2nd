68888e4db3658bf421dacab0f7528ed197e241a7
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-07 13:49:51 +0900

    [#7 클래스인라인 ex] 1,2step - shippingCompany2
    - 위임함수를 만들고
    - 클라이언트가 이를 호출하도록

--- ch7/7-6_inlineClass.js
+++ ch7/7-6_inlineClass.js
@@ -1,6 +1,4 @@
 class TrackingInformation {
-    get shippingCompany() {return this._shippingCompany;} //배송 회사
-    set shippingCompany(arg) {this._shippingCompany=arg;}
     get trackingNumber() {return this._trackingNumber;}
     set trackingNumber(arg) {this._trackingNumber=arg;}
     get display() {
@@ -8,6 +6,7 @@ class TrackingInformation {
     }
 }
 class Shipment {
+    get shippingCompany() {return this._trackingInformation.shippingCompany;}
     set shippingCompany(arg) {this._trackingInformation.shippingCompany=arg}
     get trackingInfo(){return this._trackingInformation.display;}
     get trackingInformation() {return this._trackingInformation;}
