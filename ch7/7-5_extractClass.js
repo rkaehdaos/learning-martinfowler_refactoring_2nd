1cd74da8720068b617240cf4ecd899edc16b2ac9
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-07 11:39:55 +0900

    [#7 클래스추출 ex] 3. 원래 클래스 Person 생성자에서 새 클래스 전화번호 인스턴스도 함께 생성

--- ch7/7-5_extractClass.js
+++ ch7/7-5_extractClass.js
@@ -2,6 +2,9 @@ class TelephoneNumber {
 
 }
 class Person {
+    constructor() {
+        this._telephoneNumber = new TelephoneNumber();
+    }
     get name() {return this._name;}
     set name(arg) {this._name=arg;}
     get telephoneNumber() {return `(${this.officeAreaCode}) ${this.officeNumber}`;}
