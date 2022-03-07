4780c081c93d778cb478dc445200fee3ee59156c
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-07 11:45:33 +0900

    [#7 클래스추출 ex] 4. 필드 옮기기:officeAreaCode

--- ch7/7-5_extractClass.js
+++ ch7/7-5_extractClass.js
@@ -1,4 +1,6 @@
 class TelephoneNumber {
+    get officeAreaCode() {return this._officeAreaCode;}
+    set officeAreaCode(arg) {this._officeAreaCode=arg;}
 
 }
 class Person {
@@ -8,8 +10,8 @@ class Person {
     get name() {return this._name;}
     set name(arg) {this._name=arg;}
     get telephoneNumber() {return `(${this.officeAreaCode}) ${this.officeNumber}`;}
-    get officeAreaCode() {return this._officeAreaCode;}
-    set officeAreaCode(arg) {this._officeAreaCode=arg;}
+    get officeAreaCode() {return this._telephoneNumber.officeAreaCode;}
+    set officeAreaCode(arg) {this._telephoneNumber.officeAreaCode=arg;}
     get officeNumber() {return this._officeNumber}
     set officeNumber(arg) {this._officeNumber=arg;}
 }
