6c1e2523fc71ae2a9c317607a6ef406a20c91de6
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-07 15:10:52 +0900

    7-7 위임 숨기기 PR #30
    * [#7 위임숨기기ex] 사람과 부서
    
    * [#7 위임숨기기ex]1. 부서를 숨기고 위임메서드 생성
    
    * [#7 위임숨기기ex]2. 모든 클라이언트들이 위임 메서드를 사용하도록 수정
    
    * [#7 위임숨기기ex]3. 접근자 삭제 - 1에서 이미

---
+++ ch7/7-7_hideDelegate.spec.js
@@ -0,0 +1,21 @@
+import {expect} from "chai";
+import {Person, Department} from "./7-7_hideDelegate.js";
+
+describe('7-7위임 숨기기', () => {
+    it('클라이언트는 부서가 관리자 정보를 제공한다는 사실을 알아야 한다', () => {
+        //given
+        const aDepartment = new Department();
+        aDepartment.chargeCode ='000111';
+        aDepartment.manager = 'David';
+
+        const aPerson = new Person('Ahn');
+        aPerson.department = aDepartment;
+
+        //when
+        const managerResult = aPerson.manager; //요기
+
+        //then
+        expect(managerResult).equals('David');
+
+    });
+});
