0fb4fd9aecb72823a02d3b0a7d52e6668976f56c
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-10 12:17:44 +0900

    [#8 중첩함수 최상위 옮기기] 최상위 함수는 가시성 높으므로 신중한 네이밍
    - trackSummary()의 같은 이름 변수가 덮어써서 불가능
    - 변수를 남길 이유가 없으니 인라인 한다
    - 인라인후 다시 네이밍

--- ch8/8-1_moveNestedFuncToRoot.js
+++ ch8/8-1_moveNestedFuncToRoot.js
@@ -1,17 +1,16 @@
 function trackSummary(points) {
     const totalTime = calculateTime();
-    const totalDistance = top_calculateDistance(points);
-    const pace = totalTime / 60 / totalDistance; //km당 xxx시간 pace
+    const pace = totalTime / 60 / totalDistance(points); //km당 xxx시간 pace
     return {
         time: totalTime,
-        distance: totalDistance,
+        distance: totalDistance(points),
         pace: pace
     };
 
     // 총 시간 계산
     function calculateTime() {return 300;} //대충 5시간
 }
-function top_calculateDistance(points) { //새로운 임시 이름
+function totalDistance(points) { //새로운 임시 이름
     let result = 0;
     for (let i = 1; i < points.length; i++) {
         result += distance(points[i - 1], points[i]);
