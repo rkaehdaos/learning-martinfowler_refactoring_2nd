488855105e5d423e6faea291f4ccfc71621ff274
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-10 12:19:10 +0900

    [#8 중첩함수 최상위 옮기기] 다른 함수 최상위
    -

--- ch8/8-1_moveNestedFuncToRoot.js
+++ ch8/8-1_moveNestedFuncToRoot.js
@@ -16,24 +16,25 @@ function totalDistance(points) { //새로운 임시 이름
         result += distance(points[i - 1], points[i]);
     }
     return result;
-    function distance(p1, p2) {
-        // 하버사인 공식 https://kayuse88.github.io/haversine
-        const EARTH_RADIUS = 6371; // km
-
-        const deltaLatitude = Math.abs(radians(p1.lat - p2.lat));
-        const deltaLongitude = Math.abs(radians(p1.lon - p2.lon));
-
-        const sinDeltaLat = Math.sin(deltaLatitude / 2);
-        const sinDeltaLng = Math.sin(deltaLongitude / 2);
-        const squareRoot = Math.sqrt(
-            sinDeltaLat * sinDeltaLat +
-            Math.cos(radians(p1.lat)) * Math.cos(radians(p2.lat)) * sinDeltaLng * sinDeltaLng);
-
-        const result = 2 * EARTH_RADIUS * Math.asin(squareRoot);
-        return result;
-    }
-    function radians(degrees) {return degrees * Math.PI / 180;}
 }
+function distance(p1, p2) {
+    // 하버사인 공식 https://kayuse88.github.io/haversine
+    const EARTH_RADIUS = 6371; // km
+
+    const deltaLatitude = Math.abs(radians(p1.lat - p2.lat));
+    const deltaLongitude = Math.abs(radians(p1.lon - p2.lon));
+
+    const sinDeltaLat = Math.sin(deltaLatitude / 2);
+    const sinDeltaLng = Math.sin(deltaLongitude / 2);
+    const squareRoot = Math.sqrt(
+        sinDeltaLat * sinDeltaLat +
+        Math.cos(radians(p1.lat)) * Math.cos(radians(p2.lat)) * sinDeltaLng * sinDeltaLng);
+
+    const result = 2 * EARTH_RADIUS * Math.asin(squareRoot);
+    return result;
+}
+
+function radians(degrees) {return degrees * Math.PI / 180;}
 
 export {trackSummary};
 
