8e229390605a0bce66b995f1b3d6b685f69ee647
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-10 13:58:50 +0900

    [#8 함수옮기기2] 계좌 예제

---
+++ ch8/8-1_moveFuncToOtherClass.js
@@ -0,0 +1,35 @@
+class Account {
+    constructor(accountType, daysOverdrawn) {
+        this.type = accountType;
+        this._daysOverdrawn = daysOverdrawn;
+    }
+    // 은행 이자 계산
+    get bankCharge() {
+        let result = 4.5;
+        if (this._daysOverdrawn > 0) result += this.overdraftCharge;
+        return result;
+    }
+
+    // 초과 인출 이자 계산
+    get overdraftCharge() {
+        if (this.type.isPremium) {
+            const baseCharge = 10;
+            if (this._daysOverdrawn <= 7)
+                return baseCharge;
+            else
+                return baseCharge + (this._daysOverdrawn - 7) * 0.85;
+        } else
+            return this._daysOverdrawn * 1.75;
+    }
+}
+
+class AccountType {
+    constructor(type) {
+        this._type = type;
+    }
+    get isPremium() {
+        return this._type === 'Premium';
+    }
+}
+
+export {Account,AccountType};
---
+++ ch8/8-1_moveFuncToOtherClass.spec.js
@@ -0,0 +1,28 @@
+import {expect} from "chai";
+import {Account,AccountType} from "./8-1_moveFuncToOtherClass.js";
+
+describe('8-1다른클래스로옮기기', () => {
+    it('초과 없으면 모든 계좌는 4.5', () =>{
+
+        expect(new Account(new AccountType('NonPremium'), 0).bankCharge).equals(4.5);
+        expect(new Account(new AccountType('Premium'), 0).bankCharge).equals(4.5);
+    });
+    it('일반 구좌 초과 테스트:1일당 1.75', () => {
+        expect(new Account(new AccountType('NonPremium'), 1).bankCharge).equals(6.25);
+        expect(new Account(new AccountType('NonPremium'), 2).bankCharge).equals(8.00);
+        expect(new Account(new AccountType('NonPremium'), 3).bankCharge).equals(9.75);
+        expect(new Account(new AccountType('NonPremium'), 100).bankCharge).equals(179.5);
+    });
+
+    it('프리미엄:단기는 더 비싸지만 연체가 오래될 수록 유리', () => {
+        // 7이하시 14.5 유지
+        for (let i = 1; i <= 7; i++)
+            expect(new Account(new AccountType('Premium'), i).bankCharge).equals(14.5);
+        // 8부터 0.85씩
+        expect(new Account(new AccountType('Premium'), 8).bankCharge).equals(15.35);
+        expect(new Account(new AccountType('Premium'), 9).bankCharge).equals(16.2);
+        expect(new Account(new AccountType('Premium'), 10).bankCharge).equals(17.05);
+        expect(new Account(new AccountType('Premium'), 100).bankCharge).equals(93.55);
+    });
+
+});
