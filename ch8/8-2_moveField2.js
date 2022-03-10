23a3d7cdb097ca7432e794fe6996d1009e06b5d6
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-10 17:57:24 +0900

    [#8 필드옮기기2] 8step - 소스 필드 제거

--- ch8/8-2_moveField2.js
+++ ch8/8-2_moveField2.js
@@ -1,10 +1,7 @@
-import assert from "assert";
 class Account {
-    constructor(number, type, interestRate) {
+    constructor(number, type) {
         this._number = number;
         this._type = type;
-        assert(interestRate === this._type.interestRate);
-        this._interestRate = interestRate;
     }
     get interestRate() {return this._type.interestRate;}
 }
--- ch8/8-2_moveField2.spec.js
+++ ch8/8-2_moveField2.spec.js
@@ -4,7 +4,7 @@ import {Account,AccountType} from "./8-2_moveField2.js";
 describe('8-2공유객체로이동', () => {
     it('basic', () => {
         const accountTypeA = new AccountType('A', 2.5);
-        const account = new Account(15000, accountTypeA, 2.5);
+        const account = new Account(15000, accountTypeA);
         expect(account.interestRate).equals(2.5);
     });
 });
