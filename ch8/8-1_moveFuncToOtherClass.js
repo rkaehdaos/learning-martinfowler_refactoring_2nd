2a3ea77e39702fd81f32cd823b66dc0bf90226d5
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-10 14:31:09 +0900

    [#8 함수옮기기2] 9.소스 컨텍스트에서 가져와야할 데이터가 많다면? 함수 복사부터 다시

--- ch8/8-1_moveFuncToOtherClass.js
+++ ch8/8-1_moveFuncToOtherClass.js
@@ -3,12 +3,23 @@ class Account {
         this.type = accountType;
         this._daysOverdrawn = daysOverdrawn;
     }
-
     // 은행 이자 계산
     get bankCharge() {
         let result = 4.5;
-        if (this._daysOverdrawn > 0) result += this.type.overdraftCharge(this._daysOverdrawn);
+        if (this._daysOverdrawn > 0) result += this.overdraftCharge;
         return result;
+    }
+
+    // 초과 인출 이자 계산
+    get overdraftCharge() {
+        if (this.type.isPremium) {
+            const baseCharge = 10;
+            if (this._daysOverdrawn <= 7)
+                return baseCharge;
+            else
+                return baseCharge + (this._daysOverdrawn - 7) * 0.85;
+        } else
+            return this._daysOverdrawn * 1.75;
     }
 }
 
@@ -16,21 +27,20 @@ class AccountType {
     constructor(type) {
         this._type = type;
     }
-
     get isPremium() {
         return this._type === 'Premium';
     }
 
-    overdraftCharge(daysOverdrawn) {
+    overdraftCharge() {
         if (this.isPremium) {
             const baseCharge = 10;
-            if (daysOverdrawn <= 7)
+            if (this.daysOverdrawn <= 7)
                 return baseCharge;
             else
-                return baseCharge + (daysOverdrawn - 7) * 0.85;
+                return baseCharge + (this._daysOverdrawn - 7) * 0.85;
         } else
-            return daysOverdrawn * 1.75;
+            return this._daysOverdrawn * 1.75;
     }
 }
 
-export {Account, AccountType};
+export {Account,AccountType};
