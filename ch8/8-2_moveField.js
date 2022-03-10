61553d0bed7e802f4f0e544f9aabc6374aa2089f
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-10 16:37:14 +0900

    [#8 필드옮기기1]1.변수캡슐화

--- ch8/8-2_moveField.js
+++ ch8/8-2_moveField.js
@@ -1,21 +1,20 @@
 class Customer {
     constructor(name, discountRate) {
         this._name = name;
-        this._discountRate = discountRate; //할인율
+        this._setDiscountRate(discountRate); //할인율
         this._contract = new CustomerContract(this.dateToday());
     }
 
-    get discountRate() {
-        return this._discountRate;
-    }
+    get discountRate() {return this._discountRate;}
+    _setDiscountRate(aNumber) {this._discountRate=aNumber;}
 
     becomePreferred() {
-        this._discountRate += 0.03;
+        this._setDiscountRate(this.discountRate + 0.03);
         // 다른 멋진 일들
     }
 
     applyDiscount(amount) {
-        return amount.subtract(amount.multiply(this._discountRate));
+        return amount.subtract(amount.multiply(this.discountRate));
     }
 
     dateToday() {
