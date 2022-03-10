24ce2c4ea85209adbe7277de269d02e1f3d5cd9e
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-10 14:35:16 +0900

    [#8 함수옮기기2] 9.소스 컨텍스트에서 가져와야할 데이터가 많다면? 인라인화

--- ch8/8-1_moveFuncToOtherClass.js
+++ ch8/8-1_moveFuncToOtherClass.js
@@ -6,26 +6,10 @@ class Account {
     // 은행 이자 계산
     get bankCharge() {
         let result = 4.5;
-        if (this._daysOverdrawn > 0) result += this.overdraftCharge;
+        if (this._daysOverdrawn > 0) result += this.type.overdraftCharge(this);
         return result;
     }
 
-    // 초과 인출 이자 계산
-    get overdraftCharge() {
-/*
-
-        if (this.type.isPremium) {
-            const baseCharge = 10;
-            if (this._daysOverdrawn <= 7)
-                return baseCharge;
-            else
-                return baseCharge + (this._daysOverdrawn - 7) * 0.85;
-        } else
-            return this._daysOverdrawn * 1.75;
-*/
-        return this.type.overdraftCharge(this);
-
-    }
 }
 
 class AccountType {
