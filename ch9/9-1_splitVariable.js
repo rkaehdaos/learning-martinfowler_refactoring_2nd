2d8a9d3f29e0cba1e0974088eab098d58445c55a
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-11 16:08:12 +0900

    [#9 변수쪼개기] 1.변수에 새로운 이름 지어주기 - 현재 테스트 실패

--- ch9/9-1_splitVariable.js
+++ ch9/9-1_splitVariable.js
@@ -1,6 +1,6 @@
 export function distanceTravelled(scenario, time) {
     let result;
-    let acc = scenario.primaryForce / scenario.mass; //가속도(a) = 힘(F) / 질량(m)
+    let primaryAcceration = scenario.primaryForce / scenario.mass; //가속도(a) = 힘(F) / 질량(m)
     let primaryTime = Math.min(time, scenario.delay);
     result = 0.5 * acc * primaryTime * primaryTime; // 전파된 거리
     let secondaryTime = time - scenario.delay;
