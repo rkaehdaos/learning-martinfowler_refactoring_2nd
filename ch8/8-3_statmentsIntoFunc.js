9244df2c905d76cb20681ea31a8a04097125a27c
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-11 09:25:13 +0900

    8-3 Move Statement into Func PR #37
    * [#8 문장함수로옮기기1] 사진관련data->html
    
    * [#8 문장함수로옮기기1] 3step - 호출자중 하나에 함수 추출하기 적용
    
    * [#8 문장함수로옮기기1] 4 - 다른 호출자들도 새로운 함수를 호출하도록 수정
    
    * [#8 문장함수로옮기기1] 5 - emitPhotoData() 인라인 후 테스트 성공
    
    * [#8 문장함수로옮기기1] 5 - 원래 함수 제거
    
    * [#8 문장함수로옮기기1] 6 - 새함수 네이밍

---
+++ ch8/8-3_statmentsIntoFunc.js
@@ -0,0 +1,26 @@
+function renderPerson(person) {
+    const result = [];
+    result.push(`<p>${person.name}</p>`);
+    result.push(renderPhoto(person.photo));
+    result.push(emitPhotoData(person.photo));
+    return result.join("\n");
+}
+
+function photoDiv(p) {
+    return [
+        "<div>",
+        emitPhotoData(p),
+        "</div>",
+    ].join("\n");
+}
+
+function emitPhotoData(p) {
+    return [
+        `<p>title: ${p.title}</p>`,
+        `<p>location: ${p.location}</p>`,
+        `<p>date: ${p.date.toDateString()}</p>`
+    ].join("\n");
+}
+function renderPhoto(aPhoto) {return "";}
+
+export {renderPerson, photoDiv};
