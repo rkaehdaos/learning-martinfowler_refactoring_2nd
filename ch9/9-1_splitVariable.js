a01ebd55cd26491abdca23fa621ca4544ec25eb5
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-11 16:35:45 +0900

    [#9 변수쪼개기] calcDistanceFromAccAndTime 3항

--- ch9/9-1_splitVariable.js
+++ ch9/9-1_splitVariable.js
@@ -2,17 +2,17 @@ export function distanceTravelled(scenario, time) {
 
     const primaryAcceleration = scenario.primaryForce / scenario.mass;
     let primaryTime = Math.min(time, scenario.delay);
-    let result = calcDistanceFromAccAndTime(primaryAcceleration, primaryTime);
+    let result = calcDistanceFromAccAndTime(primaryAcceleration, primaryTime, 0);
     let secondaryTime = time - scenario.delay;
     if (secondaryTime > 0) {
         let primaryVelocity = primaryAcceleration * scenario.delay;
         const secondaryAcceleration = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
-        result += primaryVelocity * secondaryTime + calcDistanceFromAccAndTime(secondaryAcceleration, secondaryTime);
+        result += calcDistanceFromAccAndTime(secondaryAcceleration, secondaryTime, primaryVelocity);
     }
     return result;
 
-    function calcDistanceFromAccAndTime(acc, time) {
-        return 0.5 * acc * time * time;
+    function calcDistanceFromAccAndTime(acc, time, currentVelocity) {
+        return currentVelocity * time + 0.5 * acc * time * time;
     }
 
 }
