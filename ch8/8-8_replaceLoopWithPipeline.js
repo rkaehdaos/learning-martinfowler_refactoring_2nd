ff14e20679b270f5067010da205497797856061f
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-11 14:44:14 +0900

    [#8 루프를반복문으로대체] 2. 레코드 생성 - map()

--- ch8/8-8_replaceLoopWithPipeline.js
+++ ch8/8-8_replaceLoopWithPipeline.js
@@ -4,10 +4,12 @@ function acquireData(input) {
     const loopItems = lines
         .slice(1)
         .filter(line => line.trim() !== "")
-        .map(line => line.split(",")).filter(record =>record[1].trim() === "India");
+        .map(line => line.split(","))
+        .filter(record =>record[1].trim() === "India")
+        .map(record => ({city: record[0].trim(), phone: record[2].trim()}));
     for (const line of loopItems) {
         const record = line;
-        result.push({city: record[0].trim(), phone: record[2].trim()});
+        result.push(line);
     }
     return result;
 }
