eaf3cf9e084d9454bf4761a73902418b9bcba281
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-11 13:43:14 +0900

    [#8 반복문쪼개기] 더 가다듬기 - 함수추출 - youngest()

--- ch8/8-7_splitLoop.js
+++ ch8/8-7_splitLoop.js
@@ -1,16 +1,17 @@
 function reportYoungestAgeAndTotalSalary(people) {
 
-    let youngest = people[0] ? people[0].age : Infinity;
-    for (const p of people) {
-        if (p.age < youngest) youngest = p.age;
-    }
-
-    return `최연소: ${youngest}, 총 급여: ${totalSalary()}`;
+    return `최연소: ${youngest()}, 총 급여: ${totalSalary()}`;
 
     function totalSalary() {
         let totalSalary = 0;
         for (const p of people) totalSalary += p.salary;
         return totalSalary;
+    }
+
+    function youngest() {
+        let youngest = people[0] ? people[0].age : Infinity;
+        for (const p of people) if (p.age < youngest) youngest = p.age;
+        return youngest;
     }
 
 }
