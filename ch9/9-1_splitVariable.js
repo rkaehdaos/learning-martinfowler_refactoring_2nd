07ba87fbc7ea61005159c5414124debb27e78480
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-11 16:21:15 +0900

    [#9 변수쪼개기] 거리 계산 중복 제거

--- ch9/9-1_splitVariable.js
+++ ch9/9-1_splitVariable.js
@@ -1,14 +1,18 @@
 export function distanceTravelled(scenario, time) {
-    let result;
-    const primaryAcceration = scenario.primaryForce / scenario.mass; //가속도(a) = 힘(F) / 질량(m)
+
+    const primaryAcceration = scenario.primaryForce / scenario.mass;
     let primaryTime = Math.min(time, scenario.delay);
-    result = 0.5 * primaryAcceration * primaryTime * primaryTime; // 전파된 거리
+    let result = calcDistanceFromAccAndTime(primaryAcceration, primaryTime);
     let secondaryTime = time - scenario.delay;
     if (secondaryTime > 0) {
         let primaryVelocity = primaryAcceration * scenario.delay;
         const secondaryAcceration = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
-        result += primaryVelocity * secondaryTime + 0.5 * secondaryAcceration * secondaryTime * secondaryTime;
+        result += primaryVelocity * secondaryTime + calcDistanceFromAccAndTime(secondaryAcceration, secondaryTime);
     }
     return result;
+
+    function calcDistanceFromAccAndTime(acc, time) {
+        return 0.5 * acc * time * time;
+    }
 
 }
