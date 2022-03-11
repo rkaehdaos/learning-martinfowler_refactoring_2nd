70c9cfce16c8bc15ca9f25a02231054d7ae06baf
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-11 16:09:30 +0900

    [#9 변수쪼개기] 3. 번째 대입 전까지 모든 참조를 새로운 이름으로 - 현재 테스트 실패

--- ch9/9-1_splitVariable.js
+++ ch9/9-1_splitVariable.js
@@ -2,10 +2,10 @@ export function distanceTravelled(scenario, time) {
     let result;
     const primaryAcceration = scenario.primaryForce / scenario.mass; //가속도(a) = 힘(F) / 질량(m)
     let primaryTime = Math.min(time, scenario.delay);
-    result = 0.5 * acc * primaryTime * primaryTime; // 전파된 거리
+    result = 0.5 * primaryAcceration * primaryTime * primaryTime; // 전파된 거리
     let secondaryTime = time - scenario.delay;
     if (secondaryTime > 0) {
-        let primaryVelocity = acc * scenario.delay;
+        let primaryVelocity = primaryAcceration * scenario.delay;
         acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
         result += primaryVelocity * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime;
     }
