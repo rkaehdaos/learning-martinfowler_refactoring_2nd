59648f9c1055528f9db7e7523ac8cdf69369ab57
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-11 17:32:39 +0900

    9-1 split variable ex1 PR #42
    * [#9 변수쪼개기] 음식전파거리계산 코드
    
    * [#9 변수쪼개기] 1.변수에 새로운 이름 지어주기
    
    * [#9 변수쪼개기] 2. const로 불변으로 만든다
    
    * [#9 변수쪼개기] 3. 번째 대입 전까지 모든 참조를 새로운 이름으로 - 현재 테스트 실패
    
    * [#9 변수쪼개기] 4. 두번째 대입할 때 변수 다시 선언 - 테스트 성공
    
    * [#9 변수쪼개기] 2번째 대입처리 - 1) 네이밍
    
    * [#9 변수쪼개기] 2번째 대입처리 - 2) 불변
    
    * [#9 변수쪼개기] 거리 계산 중복 제거
    
    * [#9 변수쪼개기] 오타 수정
    
    * [#9 변수쪼개기] calcDistanceFromAccAndTime 3항
    
    * [#9 변수쪼개기] calcAccFromForceAndMass 적용
    
    * [#9 변수쪼개기] calcAccFromForceAndMass 적용2 - 주석제거
    
    * [#9 변수쪼개기] 함수 외부 빼기
    
    * [#9 변수쪼개기] 변수명 수정
    
    * [#9 변수쪼개기] 변수scope

---
+++ ch9/9-1_splitVariable.js
@@ -0,0 +1,21 @@
+export function distanceTravelled(scenario, time) {
+
+    const primaryAcceleration = calcAccFromForceAndMass(scenario.primaryForce, scenario.mass);
+    const primaryTime = Math.min(time, scenario.delay);
+    let distance = calcDistanceFromAccAndTime(primaryAcceleration, primaryTime);
+    const secondaryTime = time - scenario.delay;
+    if (secondaryTime > 0) {
+        const primaryVelocity = primaryAcceleration * scenario.delay;
+        const secondaryAcceleration = calcAccFromForceAndMass(scenario.secondaryForce, scenario.mass, primaryAcceleration);
+        distance += calcDistanceFromAccAndTime(secondaryAcceleration, secondaryTime, primaryVelocity);
+    }
+    return distance;
+}
+
+function calcDistanceFromAccAndTime(acc, time, currentVelocity = 0) {
+    return currentVelocity * time + 0.5 * acc * time * time;
+}
+
+function calcAccFromForceAndMass(force, mass, currentAcc = 0) {
+    return currentAcc + force / mass;
+}
