091e1d98e4fe3385c098e0f96a7d6d520cc841af
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-11 17:08:05 +0900

    [#9 변수쪼개기] 변수명 수정

--- ch9/9-1_splitVariable.js
+++ ch9/9-1_splitVariable.js
@@ -2,14 +2,14 @@ export function distanceTravelled(scenario, time) {
 
     const primaryAcceleration = calcAccFromForceAndMass(scenario.primaryForce, scenario.mass);
     let primaryTime = Math.min(time, scenario.delay);
-    let result = calcDistanceFromAccAndTime(primaryAcceleration, primaryTime);
+    let distance = calcDistanceFromAccAndTime(primaryAcceleration, primaryTime);
     let secondaryTime = time - scenario.delay;
     if (secondaryTime > 0) {
-        let primaryVelocity = primaryAcceleration * scenario.delay;
+        const primaryVelocity = primaryAcceleration * scenario.delay;
         const secondaryAcceleration = calcAccFromForceAndMass(scenario.secondaryForce, scenario.mass, primaryAcceleration);
-        result += calcDistanceFromAccAndTime(secondaryAcceleration, secondaryTime, primaryVelocity);
+        distance += calcDistanceFromAccAndTime(secondaryAcceleration, secondaryTime, primaryVelocity);
     }
-    return result;
+    return distance;
 
 }
 function calcDistanceFromAccAndTime(acc, time, currentVelocity = 0) {
