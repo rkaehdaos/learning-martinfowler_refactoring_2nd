05ad87583e27f43928d184f761958ccd59364a46
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-11 12:31:36 +0900

    [#8 문장 슬라이드] ex

---
+++ ch8/8-6.js
@@ -0,0 +1,13 @@
+const pricingPlan = retrievePricingPlan();
+const order = retrieveOrder();
+const baseCharge = pricingPlan.base;
+let charge;
+const chargePerUnit = pricingPlan.unit;
+const units = order.units;
+let discount;
+charge = baseCharge + units * chargePerUnit;
+let discountTableUnits = Math.max(units - pricingPlan.discountThreshold, 0);
+discount = discountTableUnits * pricingPlan.discountFactor;
+if (order.isRepeat) discount += 20;
+charge = charge - discount;
+chargeOrder(charge);
