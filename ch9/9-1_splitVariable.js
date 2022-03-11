c1d56ac9af8e5c882be22f0ddff0878a306a2aa3
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-11 16:52:20 +0900

    [#9 변수쪼개기] calcAccFromForceAndMass 적용

--- ch9/9-1_splitVariable.js
+++ ch9/9-1_splitVariable.js
@@ -1,18 +1,23 @@
 export function distanceTravelled(scenario, time) {
 
-    const primaryAcceleration = scenario.primaryForce / scenario.mass;
+    const primaryAcceleration = calcAccFromForceAndMass(scenario.primaryForce, scenario.mass);
     let primaryTime = Math.min(time, scenario.delay);
-    let result = calcDistanceFromAccAndTime(primaryAcceleration, primaryTime, 0);
+    let result = calcDistanceFromAccAndTime(primaryAcceleration, primaryTime);
     let secondaryTime = time - scenario.delay;
     if (secondaryTime > 0) {
         let primaryVelocity = primaryAcceleration * scenario.delay;
-        const secondaryAcceleration = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
+        // const secondaryAcceleration = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
+        const secondaryAcceleration = calcAccFromForceAndMass(scenario.secondaryForce, scenario.mass, primaryAcceleration);
         result += calcDistanceFromAccAndTime(secondaryAcceleration, secondaryTime, primaryVelocity);
     }
     return result;
 
-    function calcDistanceFromAccAndTime(acc, time, currentVelocity) {
+    function calcDistanceFromAccAndTime(acc, time, currentVelocity = 0) {
         return currentVelocity * time + 0.5 * acc * time * time;
+    }
+
+    function calcAccFromForceAndMass(force, mass, currentAcc=0){
+        return currentAcc + force / mass;
     }
 
 }
