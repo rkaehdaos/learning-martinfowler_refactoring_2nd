ae66e50307059f8cd9ea50781fd8e168eaa27b66
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-11 14:51:46 +0900

    [#8 루프를반복문으로대체] 더 가다듬기 - 변수명 네이밍

--- ch8/8-8_replaceLoopWithPipeline.js
+++ ch8/8-8_replaceLoopWithPipeline.js
@@ -4,8 +4,8 @@ function acquireData(input) {
         .slice(1)
         .filter(line => line.trim() !== "")
         .map(line => line.split(","))
-        .filter(record => record[1].trim() === "India")
-        .map(record => ({city: record[0].trim(), phone: record[2].trim()}));
+        .filter(fields => fields[1].trim() === "India")
+        .map(fields => ({city: fields[0].trim(), phone: fields[2].trim()}));
 }
 
 export {acquireData};
