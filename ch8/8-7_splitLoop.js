df86c8a039529c608c2bd7266c1b0e3c25ec73d6
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-11 14:02:47 +0900

    [#8 반복문쪼개기] 더 가다듬기 - 알고리즘 교체
    -

--- ch8/8-7_splitLoop.js
+++ ch8/8-7_splitLoop.js
@@ -2,16 +2,14 @@ function reportYoungestAgeAndTotalSalary(people) {
 
     return `최연소: ${youngest()}, 총 급여: ${totalSalary()}`;
 
+
     function totalSalary() {
         return people.reduce((total, p) => total + p.salary, 0);
     }
 
     function youngest() {
-        let youngest = people[0] ? people[0].age : Infinity;
-        for (const p of people) if (p.age < youngest) youngest = p.age;
-        return youngest;
+        return Math.min(...people.map(p => p.age));
     }
-
 }
 
 export {reportYoungestAgeAndTotalSalary};
