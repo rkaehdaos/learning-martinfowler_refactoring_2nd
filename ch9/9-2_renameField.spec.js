3bdc289207d9daf68b10219102f5fb9a5aaba97d
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-15 10:42:25 +0900

    [#9 필드이름바꾸기] 테스트 제대로 수정

--- ch9/9-2_renameField.spec.js
+++ ch9/9-2_renameField.spec.js
@@ -2,6 +2,9 @@ import {expect} from "chai";
 import {organization} from "./9-2_renameField.js";
 
 describe('9-2 필드 이름 바꾸기', () => {
-    it('name과 country에 다음 값 포함 ', () =>
-        expect(organization).to.deep.equal({name: "애크미 구스베리", country: "GB"}));
+    it('name과 country에 다음 값 포함 ', () => {
+        expect(organization).to.have.property('name', '애크미 구스베리');
+        expect(organization).to.have.property('country', 'GB');
+    });
+
 });
