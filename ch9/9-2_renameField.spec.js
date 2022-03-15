4647d0c0582387dcf21c8c187756f84aceae9d3d
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-15 11:24:46 +0900

    [#9 필드이름바꾸기] 6.접근자 수정(내용)
    - 생성자와 데이터가 새로운 이름 사용하게 됨
    - 이제 접근자도 수정 가능
    - 각각 접근자의 이름을 바꿔준다
    
    - 접근자를 이용하던 부분도 이름을 바꿔주는 부분 필요!

--- ch9/9-2_renameField.spec.js
+++ ch9/9-2_renameField.spec.js
@@ -3,7 +3,7 @@ import {organization} from "./9-2_renameField.js";
 
 describe('9-2 필드 이름 바꾸기', () => {
     it('name과 country에 다음 값 포함 ', () => {
-        expect(organization).to.have.property('name', '애크미 구스베리');
+        expect(organization).to.have.property('title', '애크미 구스베리');
         expect(organization).to.have.property('country', 'GB');
     });
 
