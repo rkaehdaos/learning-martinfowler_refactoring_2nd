1e6ba72f8b72f788cc6d253f8d29f61cdb968bb2
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-15 11:26:20 +0900

    9-2 rename Field PR #44
    * [#9 필드이름바꾸기] 7-1레코드캡슐화와 동일한 예시
    
    * [#9 필드이름바꾸기] 테스트 제대로 수정
    
    * [#9 필드이름바꾸기] 맨처음 7-1 예시 성공 확인
    
    * [#9 필드이름바꾸기] 2.레코드 캡슐화
    
    * [#9 필드이름바꾸기] 3-1. private 필드명 변경, getter 수정
    
    * [#9 필드이름바꾸기] 3-2. 생성자에서 "title"도 받아들일 수 있게 수정
    
    * [#9 필드이름바꾸기] 3-3. 생성자 호출부를 찾아 바꾼 이름 title 사용하도록 하나씩 수정
    
    * [#9 필드이름바꾸기] 3-3. 모두 수정 후에 생성자에서 기존 이름 사용 부분 제거
    
    * [#9 필드이름바꾸기] 6.접근자 수정(내용)

---
+++ ch9/9-2_renameField.js
@@ -0,0 +1,14 @@
+class Organization {
+    constructor(data) {
+        this._title = data.title;
+        this._country = data.country;
+    }
+    get title(){return this._title;}
+    set title(arg){this._title=arg;}
+    get country(){return this._country;}
+    set country(arg){this._country=arg;}
+}
+
+const organization = new Organization({title: "애크미 구스베리", country: "GB"});
+
+export {organization};
