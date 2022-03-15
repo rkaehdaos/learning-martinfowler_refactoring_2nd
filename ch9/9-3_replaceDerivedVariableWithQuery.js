d03162770368e8bed943cc3ce95b353cf900fcc1
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-15 16:11:57 +0900

    [#9 파생변수질의함수로1] 2.해당 변수를 계싼해주는 함수를 만든다

--- ch9/9-3_replaceDerivedVariableWithQuery.js
+++ ch9/9-3_replaceDerivedVariableWithQuery.js
@@ -1,16 +1,24 @@
+import {assert} from "chai";
+
 class ProductionPlan {
-    constructor(production) {
-        this._production = production;
+    constructor() {
+        this._production = 0;
         this._adjustments = [];
     }
 
-    get production() {return this._production;}
+    get production() {
+        // assert(this._production === this.calcaulatedProduction);
+        return this._production;
+    }
 
     applyAdjustment(anAdjustment) {
         this._adjustments.push(anAdjustment);
         this._production += anAdjustment.amount;
     }
 
+    get calcaulatedProduction() {
+        return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
+    }
 
 }
 
