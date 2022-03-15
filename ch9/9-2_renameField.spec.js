a52c7bb77a15190e52ebea396bb21cb2ac8ec5f4
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-15 10:08:05 +0900

    [#9 필드이름바꾸기] 7-1레코드캡슐화와 동일한 예시

---
+++ ch9/9-2_renameField.spec.js
@@ -0,0 +1,7 @@
+import {expect} from "chai";
+import {organization} from "./9-2_renameField.js";
+
+describe('9-2 필드 이름 바꾸기', () => {
+    it('name과 country에 다음 값 포함 ', () =>
+        expect(organization).to.deep.equal({name: "애크미 구스베리", country: "GB"}));
+});
