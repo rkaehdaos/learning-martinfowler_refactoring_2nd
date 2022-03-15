cd20a52541a6bba211ed031fe5edb3e1f757483f
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-15 11:49:52 +0900

    [#9 파생변수질의함수로] 1-1. 실제 테스트를 위한 생성자 추가

--- ch9/9-3_replaceDerivedVariableWithQuery.js
+++ ch9/9-3_replaceDerivedVariableWithQuery.js
@@ -1,5 +1,11 @@
 class ProductionPlan {
+    constructor() {
+        this._adjustments = [];
+        this._production = 0;
+    }
+
     get production() {return this._production;}
+
     applyAdjustment(anAdjustment) {
         this._adjustments.push(anAdjustment)
         this._production += anAdjustment.amount
