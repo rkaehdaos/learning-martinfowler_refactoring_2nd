38714adc12be40cf95e516c364d5e462de53aa90
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-15 17:01:01 +0900

    [#9 파생변수질의함수로2] 1. 변수쪼개기

--- ch9/9-3_replaceDerivedVariableWithQuery2.js
+++ ch9/9-3_replaceDerivedVariableWithQuery2.js
@@ -1,14 +1,16 @@
 class ProductionPlan {
     constructor(production) {
-        this._production = production;
+        this._initialProduction = production;
+        this._productionAccumulator = 0;
         this._adjustments = [];
     }
 
-    get production() {return this._production;}
+    get production() {return this._initialProduction+this._productionAccumulator;}
 
+    // 책에는 안나왔지만
     applyAdjustment(anAdjustment) {
         this._adjustments.push(anAdjustment);
-        this._production += anAdjustment.amount;
+        this._productionAccumulator += anAdjustment.amount;
     }
 
 }
