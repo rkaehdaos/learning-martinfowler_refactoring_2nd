9dc82d4fb4208105e90ae5b86859d34c67e735d0
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-16 13:53:51 +0900

    [#9 값을 참조로] 1.저장소 - Map

--- ch9/9-5_changeValueToRef.js
+++ ch9/9-5_changeValueToRef.js
@@ -1,3 +1,22 @@
+let _repositoryData;
+
+export function initialize() {
+    _repositoryData = {};
+    _repositoryData.customers = new Map();
+}
+
+export function registerCustomer(id) {
+    if (!_repositoryData.customers.has(id))
+        _repositoryData.customers.set(id, new Customer(id));
+    return findCustomer(id);
+
+}
+
+export function findCustomer(id) {
+    return _repositoryData.customers.get(id);
+}
+
+
 class Order {
     constructor(data) {
         this._number=data.number;
