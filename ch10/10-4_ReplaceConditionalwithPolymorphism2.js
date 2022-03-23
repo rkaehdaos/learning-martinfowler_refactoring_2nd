7c6a02ddd1afde581f26ed83f075e010e20a3f9d
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-23 15:48:37 +0900

    [#10 10-4 변형동작 다형성으로 표현]voyageAndHistoryLengthFactor 오버라이드1
    - 복사

--- ch10/10-4_ReplaceConditionalwithPolymorphism2.js
+++ ch10/10-4_ReplaceConditionalwithPolymorphism2.js
@@ -69,4 +69,18 @@ class ExperiencedChinaRating extends Rating {
         const result = super.captainHistoryRisk - 2;
         return Math.max(result, 0);
     }
+
+    get voyageAndHistoryLengthFactor() {
+        let result = 0;
+        if (this.voyage.zone === "중국" && this.hasChinaHistory) {
+            result += 3;
+            if (this.history.length > 10) result += 1;
+            if (this.voyage.length > 12) result += 1;
+            if (this.voyage.length > 18) result -= 1;
+        } else {
+            if (this.history.length > 8) result += 1;
+            if (this.voyage.length > 14) result -= 1;
+        }
+        return result;
+    }
 }
