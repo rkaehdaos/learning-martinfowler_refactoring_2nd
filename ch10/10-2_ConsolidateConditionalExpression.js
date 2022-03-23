c2e4cbebd52127411ea487ff39fcae14ababbe88
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-23 11:02:33 +0900

    [#10 10-2조건문통합하기] 조건들 통합후 테스트통과
    - 부수효과 없으므로 1step skip

--- ch10/10-2_ConsolidateConditionalExpression.js
+++ ch10/10-2_ConsolidateConditionalExpression.js
@@ -1,9 +1,10 @@
 function disabilityAmount(anEmployee) {
     // 검사
-    if(anEmployee.seniority<2) return 0;
-    if(anEmployee.monthDisabled >12) return 0;
-    if(anEmployee.isPartTime) return 0;
+    if ((anEmployee.seniority < 2)
+        || (anEmployee.monthDisabled > 12)) return 0;
+    if (anEmployee.isPartTime) return 0;
     // 장애 수당 계산
     return 100;
 }
+
 export {disabilityAmount}
