538c55a9d3c9f3ef6ae68239f5a430f02d6d7be3
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-23 10:29:52 +0900

    [#10 10-1조건문분해]

--- ch10/10-1_DecomposeConditional.js
+++ ch10/10-1_DecomposeConditional.js
@@ -1,5 +1,5 @@
 function calcService() {
-    if(!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd))
+    if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd))
         charge = quantity * plan.summerRate;
     else
         charge = quantity * plan.regularRate + plan.regularServiceCharge;
