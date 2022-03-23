96ac552be208945ed409f4b6778a2d4fb730207b
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-23 10:31:23 +0900

    [#10 10-1조건문분해] 1-1. 조건부분 함수 추출

--- ch10/10-1_DecomposeConditional.js
+++ ch10/10-1_DecomposeConditional.js
@@ -1,6 +1,10 @@
 function calcService() {
-    if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd))
+    if (summer())
         charge = quantity * plan.summerRate;
     else
         charge = quantity * plan.regularRate + plan.regularServiceCharge;
+
+    function summer() {
+        return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
+    }
 }
