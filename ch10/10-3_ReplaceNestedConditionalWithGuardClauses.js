5709259dd264246001c887836b4c693300715524
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-23 11:51:26 +0900

    [#10 10-3중첩조건문을보호구문으로] 1.상위부터 보호구문으로
    - 이 코드는 실제로 벌어지는 중요한 일들이 중첩된 조건에 가려 잘 보이지 않는다
    - 코드가 진짜 의도한 일이 모든 조건이 거짓일때 실행되므로
    - 이 상황에선 보호구문을 사용하면 코드 의도가 더 잘 드러난다
    - 작은 스텝으로 상위 조건부터 보호구문으로

--- ch10/10-3_ReplaceNestedConditionalWithGuardClauses.js
+++ ch10/10-3_ReplaceNestedConditionalWithGuardClauses.js
@@ -1,19 +1,16 @@
 function payAmount(employee) {
     let result;
-    if (employee.isSeparated) {
-        result = {amount: 0, resonCode: "SEP"};
+    if (employee.isSeparated) return {amount: 0, resonCode: "SEP"};
+    if (employee.isRetired) {
+        result = {amount: 0, resonCode: "RET"};
     } else {
-        if (employee.isRetired) {
-            result = {amount: 0, resonCode: "RET"};
-        } else {
-            //급여 계산로직
-            // lorem.ipsum(dolor.sitAmet);
-            // consectetur(adipiscing).elit();
-            // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-            // ut.enim.ad(minim.veniam);
-            // result = someFinalComputation();
-            result = {amount: 1000, resonCode: "OK"};
-        }
+        //급여 계산로직
+        // lorem.ipsum(dolor.sitAmet);
+        // consectetur(adipiscing).elit();
+        // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+        // ut.enim.ad(minim.veniam);
+        // result = someFinalComputation();
+        result = {amount: 1000, resonCode: "OK"};
     }
     return result;
 }
