c2b72ad7a7574ba57a57d9bcac64f484b2ffaaa9
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-23 15:29:33 +0900

    [#10 10-4 변형동작 다형성으로 표현] -  여러 함수를 클래스로 묶기 -1
    -

--- ch10/10-4_ReplaceConditionalwithPolymorphism2.js
+++ ch10/10-4_ReplaceConditionalwithPolymorphism2.js
@@ -1,4 +1,4 @@
-export function rating(voyage,history) { //투자 등급
+export function rating(voyage, history) { //투자 등급
     const vpf = voyageProfitFactor(voyage, history);
     const vr = voyageRisk(voyage);
     const chr = captainHistoryRisk(voyage, history);
@@ -35,8 +35,7 @@ function voyageProfitFactor(voyage, history) { // 수익 요인
         if (history.length > 10) result += 1;
         if (voyage.length > 12) result += 1;
         if (voyage.length > 18) result -= 1;
-    }
-    else {
+    } else {
         if (history.length > 8) result += 1;
         if (voyage.length > 14) result -= 1;
     }
@@ -48,9 +47,48 @@ class Rating {
         this.voyage = voyage;
         this.history = history;
     }
-    get value() {}
-    get voyageRisk() {}
-    get captainHistoryRisk() {}
-    get voyageProfitFactor() {}
-    get hasChinaHistory() {}
+
+    get value() {
+        const vpf = this.voyageProfitFactor;
+        const vr = this.voyageRisk;
+        const chr = this.captainHistoryRisk;
+        if (vpf * 3 > (vr + chr * 2)) return "A";
+        else return "B";
+    }
+
+    get voyageRisk() {
+        let result = 1;
+        if (this.voyage.length > 4) result += 2;
+        if (this.voyage.length > 8) result += this.voyage.length - 8;
+        if (["중국", "동인도"].includes(this.voyage.zone)) result += 4;
+        return Math.max(result, 0);
+    }
+
+    get captainHistoryRisk() {
+        let result = 1;
+        if (this.history.length < 5) result += 4;
+        result += this.history.filter(v => v.profit < 0).length;
+        if (this.voyage.zone === "중국" && this.hasChinaHistory) result -= 2;
+        return Math.max(result, 0);
+    }
+
+    get voyageProfitFactor() {
+        let result = 2;
+        if (this.voyage.zone === "중국") result += 1;
+        if (this.voyage.zone === "동인도") result += 1;
+        if (this.voyage.zone === "중국" && this.hasChinaHistory) {
+            result += 3;
+            if (this.history.length > 10) result += 1;
+            if (this.voyage.length > 12) result += 1;
+            if (this.voyage.length > 18) result -= 1;
+        } else {
+            if (this.history.length > 8) result += 1;
+            if (this.voyage.length > 14) result -= 1;
+        }
+        return result;
+    }
+
+    get hasChinaHistory() {
+        return this.history.some(v => "중국" === v.zone);
+    }
 }
