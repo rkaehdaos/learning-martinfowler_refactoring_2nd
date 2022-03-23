b7ec249445ad803038964b7d2f13d08956a66cab
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-23 11:54:12 +0900

    [#10 10-3중첩조건문을보호구문으로] result 삭제
    - 이제 result는 아무 일도 안하므로 삭제
    - 실제로는 주석 바뀐 부분 참고

--- ch10/10-3_ReplaceNestedConditionalWithGuardClauses.js
+++ ch10/10-3_ReplaceNestedConditionalWithGuardClauses.js
@@ -1,5 +1,4 @@
 function payAmount(employee) {
-    let result;
     if (employee.isSeparated) return {amount: 0, resonCode: "SEP"};
     if (employee.isRetired) return {amount: 0, resonCode: "RET"};
     //급여 계산로직
@@ -7,9 +6,8 @@ function payAmount(employee) {
     // consectetur(adipiscing).elit();
     // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
     // ut.enim.ad(minim.veniam);
-    // result = someFinalComputation();
-    result = {amount: 1000, resonCode: "OK"};
-    return result;
+    // return someFinalComputation();
+    return {amount: 1000, resonCode: "OK"};
 }
 
 export {payAmount};
