3c1e70280815240181b4fd78846e3048a5648381
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-23 12:27:56 +0900

    [#10 10-3중첩조건문을보호구문으로2] result 변수 처리 필요-인라인

--- ch10/10-3_ReplaceNestedConditionalWithGuardClauses2.js
+++ ch10/10-3_ReplaceNestedConditionalWithGuardClauses2.js
@@ -1,8 +1,6 @@
 function adjustedCapital(anInstrument) {
-
     if ((anInstrument.capital <= 0) || (anInstrument.interestRate <= 0 || anInstrument.duration <= 0)) return 0;
-    let result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
-    return result;
+    return (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
 }
 
 export {adjustedCapital};
