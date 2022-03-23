88432ec77ed0142dc694cfd55db805f39a572784
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-23 15:49:32 +0900

    [#10 10-4 변형동작 다형성으로 표현]voyageAndHistoryLengthFactor 오버라이드2
    - 각각 맞게 수정

--- ch10/10-4_ReplaceConditionalwithPolymorphism2.js
+++ ch10/10-4_ReplaceConditionalwithPolymorphism2.js
@@ -47,15 +47,9 @@ class Rating {
 
     get voyageAndHistoryLengthFactor() {
         let result = 0;
-        if (this.voyage.zone === "중국" && this.hasChinaHistory) {
-            result += 3;
-            if (this.history.length > 10) result += 1;
-            if (this.voyage.length > 12) result += 1;
-            if (this.voyage.length > 18) result -= 1;
-        } else {
-            if (this.history.length > 8) result += 1;
-            if (this.voyage.length > 14) result -= 1;
-        }
+        if (this.history.length > 8) result += 1;
+        if (this.voyage.length > 14) result -= 1;
+
         return result;
     }
 
@@ -72,15 +66,11 @@ class ExperiencedChinaRating extends Rating {
 
     get voyageAndHistoryLengthFactor() {
         let result = 0;
-        if (this.voyage.zone === "중국" && this.hasChinaHistory) {
-            result += 3;
-            if (this.history.length > 10) result += 1;
-            if (this.voyage.length > 12) result += 1;
-            if (this.voyage.length > 18) result -= 1;
-        } else {
-            if (this.history.length > 8) result += 1;
-            if (this.voyage.length > 14) result -= 1;
-        }
+        result += 3;
+        if (this.history.length > 10) result += 1;
+        if (this.voyage.length > 12) result += 1;
+        if (this.voyage.length > 18) result -= 1;
+
         return result;
     }
 }
