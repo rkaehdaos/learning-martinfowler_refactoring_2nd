886d14858781f04a4641ab294b73488ed4ad1c3c
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-23 11:05:41 +0900

    [#10 10-2조건문통합하기] 3. 고려해보고 최종식 함수 추출

--- ch10/10-2_ConsolidateConditionalExpression.js
+++ ch10/10-2_ConsolidateConditionalExpression.js
@@ -1,11 +1,15 @@
 function disabilityAmount(anEmployee) {
     // 검사
-    if ((anEmployee.seniority < 2)
-        || (anEmployee.monthDisabled > 12)
-        || (anEmployee.isPartTime)) return 0;
+    if (isNotEligibleForDisability()) return 0;
 
     // 장애 수당 계산
     return 100;
+}
+
+function isNotEligibleForDisability() { //장애 수당 적용 여부 확인
+    return (anEmployee.seniority < 2)
+        || (anEmployee.monthDisabled > 12)
+        || (anEmployee.isPartTime);
 }
 
 export {disabilityAmount}
