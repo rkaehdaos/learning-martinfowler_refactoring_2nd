547958c8ecaf02a0ae3d024969eb1101fb1ef871
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-23 10:32:21 +0900

    [#10 10-1조건문분해] 1-2. 조건에 따른 로직도 함수 추출

--- ch10/10-1_DecomposeConditional.js
+++ ch10/10-1_DecomposeConditional.js
@@ -1,10 +1,15 @@
 function calcService() {
     if (summer())
-        charge = quantity * plan.summerRate;
+        charge = summerCharge();
     else
         charge = quantity * plan.regularRate + plan.regularServiceCharge;
 
     function summer() {
         return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
     }
+
+    function summerCharge() {
+        return quantity * plan.summerRate;
+    }
+
 }
