912386cb776860e184d305624d098fb5a76798f1
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-03-23 11:52:40 +0900

    [#10 10-3중첩조건문을보호구문으로] 3. 다음조건
    - 테스트 확인해가면서 계속 진행

--- ch10/10-3_ReplaceNestedConditionalWithGuardClauses.js
+++ ch10/10-3_ReplaceNestedConditionalWithGuardClauses.js
@@ -1,17 +1,14 @@
 function payAmount(employee) {
     let result;
     if (employee.isSeparated) return {amount: 0, resonCode: "SEP"};
-    if (employee.isRetired) {
-        result = {amount: 0, resonCode: "RET"};
-    } else {
-        //급여 계산로직
-        // lorem.ipsum(dolor.sitAmet);
-        // consectetur(adipiscing).elit();
-        // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-        // ut.enim.ad(minim.veniam);
-        // result = someFinalComputation();
-        result = {amount: 1000, resonCode: "OK"};
-    }
+    if (employee.isRetired) return {amount: 0, resonCode: "RET"};
+    //급여 계산로직
+    // lorem.ipsum(dolor.sitAmet);
+    // consectetur(adipiscing).elit();
+    // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+    // ut.enim.ad(minim.veniam);
+    // result = someFinalComputation();
+    result = {amount: 1000, resonCode: "OK"};
     return result;
 }
 
