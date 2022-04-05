4e80b81098b87f53ea539d8b81c349ca0d64e2f2
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-05 18:08:51 +0900

    [#11 4.객체통째로 넘기기2] 원래 매서드는 다른 컨텍스트→함수 옮기기 수행
    - 원래 메서드가 HeatingPlan안의 메서드
    - 즉 다른 컨텍스트
    - 함수 옮기기 수행해서 똑같은 컨텍스트 안으로

--- ch11/11-4_PreserveWholeObject2.js
+++ ch11/11-4_PreserveWholeObject2.js
@@ -7,21 +7,20 @@ export class HeatingPlan {
         return (bottom >= this._temperatureRange.low)
             && (top <= this._temperatureRange.high);
     }
+    xxNewwithinRange(tempRange) {
+        const low = tempRange.low;
+        const high = tempRange.high;
+        const isWithinRange = this.withinRange(low, high);
+        return isWithinRange;
+    }
 }
 
 //호출자
 export function temperatureAlerts(aRoom, aPlan) {
     const alerts = [];
     const tempRange = aRoom.daysTempRange;
-    const isWithinRange = xxNewwithinRange(tempRange, aPlan);
+    const isWithinRange = aPlan.xxNewwithinRange(tempRange);
     if (!isWithinRange)
         alerts.push("방 온도가 지정 범위를 벗어났습니다.");
     return alerts;
-}
-
-function xxNewwithinRange(tempRange, aPlan) {
-    const low = tempRange.low;
-    const high = tempRange.high;
-    const isWithinRange = aPlan.withinRange(low, high);
-    return isWithinRange;
 }
