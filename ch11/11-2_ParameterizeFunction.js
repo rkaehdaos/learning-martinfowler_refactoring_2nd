fd015f100ca89f9f39e5a57539e08dba2b287beb
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-05 11:14:00 +0900

    [#11 2.함수 매개변수화하기] step5
    - 매개변수로 받은 값을 사용하도록 함수 본문 수정
    - 하나 수정할 때마다 테스트

--- ch11/11-2_ParameterizeFunction.js
+++ ch11/11-2_ParameterizeFunction.js
@@ -13,7 +13,7 @@ function bottomBand(usage) {
 
 //선택!
 function withinBand(usage, bottom, top) {
-    return usage > 100 ? Math.min(usage, 200) - 100 : 0;
+    return usage > bottom ? Math.min(usage, top) - 100 : 0;
 }
 
 function topBand(usage) {
