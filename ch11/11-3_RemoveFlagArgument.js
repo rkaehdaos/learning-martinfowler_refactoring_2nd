1a9044bc2430056e97945b0c6b8b6fc4491d9b04
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-05 15:56:14 +0900

    [#11 3.플래그 인수 제거하기] step1:10.1조건문 분해

--- ch11/11-3_RemoveFlagArgument.js
+++ ch11/11-3_RemoveFlagArgument.js
@@ -1,18 +1,23 @@
 export function deliveryDate(anOrder, isRush) {
-    if (isRush) {
-        let deliveryTime;
-        if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
-        else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
-        else deliveryTime = 3;
-        return anOrder.placedOn.plusDays(1 + deliveryTime);
-    } else {
-        let deliveryTime;
-        if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) deliveryTime = 2;
-        else if (["ME", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
-        else deliveryTime = 4;
-        return anOrder.placedOn.plusDays(2 + deliveryTime);
-    }
+    if (isRush) return rushDeliveryDate(anOrder);
+    else        return regularDeliveryDate(anOrder);
 
+}
+
+function rushDeliveryDate(anOrder) {
+    let deliveryTime;
+    if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
+    else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
+    else deliveryTime = 3;
+    return anOrder.placedOn.plusDays(1 + deliveryTime);
+}
+
+function regularDeliveryDate(anOrder) {
+    let deliveryTime;
+    if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) deliveryTime = 2;
+    else if (["ME", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
+    else deliveryTime = 4;
+    return anOrder.placedOn.plusDays(2 + deliveryTime);
 }
 
 export class Order {
@@ -21,12 +26,26 @@ export class Order {
         this._title = title;
         this._deliveryState = deliveryState;
         this.PlacedOn = class {
-            constructor() {this._days = 0;}
-            plusDays(arg) {return this._days=this._days+arg;}
+            constructor() {
+                this._days = 0;
+            }
+
+            plusDays(arg) {
+                return this._days = this._days + arg;
+            }
         };
-        this.placedOn= new this.PlacedOn;
+        this.placedOn = new this.PlacedOn;
     }
-    get id() {return this._id;}
-    get title() {return this._title;}
-    get deliveryState() {return this._deliveryState;}
+
+    get id() {
+        return this._id;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    get deliveryState() {
+        return this._deliveryState;
+    }
 }
