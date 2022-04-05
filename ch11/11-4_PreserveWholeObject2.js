255d566f08e63c8709e9e2a088ff838be399208c
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-05 18:04:58 +0900

    [#11 4.객체통째로 넘기기2] 함수 추출하기 - 새 메서드 생성

--- ch11/11-4_PreserveWholeObject2.js
+++ ch11/11-4_PreserveWholeObject2.js
@@ -13,10 +13,15 @@ export class HeatingPlan {
 export function temperatureAlerts(aRoom, aPlan) {
     const alerts = [];
     const tempRange = aRoom.daysTempRange;
-    const low = tempRange.low;
-    const high = tempRange.high;
-    const isWithinRange = aPlan.withinRange(low, high);
+    const isWithinRange = xxNewwithinRange(tempRange, aPlan);
     if (!isWithinRange)
         alerts.push("방 온도가 지정 범위를 벗어났습니다.");
     return alerts;
+}
+
+function xxNewwithinRange(tempRange, aPlan) {
+    const low = tempRange.low;
+    const high = tempRange.high;
+    const isWithinRange = aPlan.withinRange(low, high);
+    return isWithinRange;
 }
