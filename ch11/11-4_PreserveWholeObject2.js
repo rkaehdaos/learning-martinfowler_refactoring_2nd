e9e5913fad8940cbdc888795af1439b653b1bbad
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-05 18:00:45 +0900

    [#11 4.객체통째로 넘기기2] 처음상태로

--- ch11/11-4_PreserveWholeObject2.js
+++ ch11/11-4_PreserveWholeObject2.js
@@ -3,18 +3,18 @@ export class HeatingPlan {
         this._temperatureRange = temperatureRange;
     }
 
-    withinRange(aNumberRange) {
-        return (aNumberRange.low >= this._temperatureRange.low) &&
-            (aNumberRange.high <= this._temperatureRange.high);
+    withinRange(bottom, top) {
+        return (bottom >= this._temperatureRange.low)
+            && (top <= this._temperatureRange.high);
     }
 }
 
 //호출자
 export function temperatureAlerts(aRoom, aPlan) {
     const alerts = [];
-    if (!aPlan.withinRange(aRoom.daysTempRange))
+    const low = aRoom.daysTempRange.low;
+    const high = aRoom.daysTempRange.high;
+    if (!aPlan.withinRange(low, high))
         alerts.push("방 온도가 지정 범위를 벗어났습니다.");
     return alerts;
 }
-
-
