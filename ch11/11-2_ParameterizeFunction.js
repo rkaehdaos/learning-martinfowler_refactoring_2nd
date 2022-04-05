3e44460847133e9c39932837d0c247b0e123f848
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-05 11:12:35 +0900

    [#11 2.함수 매개변수화하기] step3+: 함수네이밍도 매개변수화된 기능에 어울리게
    함수네이밍도 매개변수화된 기능에 어울리게

--- ch11/11-2_ParameterizeFunction.js
+++ ch11/11-2_ParameterizeFunction.js
@@ -2,7 +2,7 @@ export function baseCharge(usage) {
     if (usage < 0) return usd(0);
     const amount =
         bottomBand(usage) * 0.03
-        + middleBand(usage,100,200) * 0.05
+        + withinBand(usage,100,200) * 0.05
         + topBand(usage) * 0.07;
     return usd(amount);
 }
@@ -12,7 +12,7 @@ function bottomBand(usage) {
 }
 
 //선택!
-function middleBand(usage, bottom, top) {
+function withinBand(usage, bottom, top) {
     return usage > 100 ? Math.min(usage, 200) - 100 : 0;
 }
 
