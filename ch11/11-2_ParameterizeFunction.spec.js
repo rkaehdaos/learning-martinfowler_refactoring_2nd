108eda51fe12dd0732a296c6f02e001a6d38d83e
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-05 11:05:16 +0900

    [#11 2.함수 매개변수화하기] 예제 (테스트 내가 더 추가)

---
+++ ch11/11-2_ParameterizeFunction.spec.js
@@ -0,0 +1,17 @@
+import {expect} from "chai";
+import {baseCharge} from "./11-2_ParameterizeFunction.js";
+
+describe('11-2 baseCharge', () => {
+    it('usage가 0이면 결과도 0', () =>
+        expect(baseCharge(0)).to.eql({currency: "$", currencyName: "USD", value: 0}));
+    it('10이면 bottomBand만 적용되서 0.3 ', () =>
+        expect(baseCharge(10)).to.eql({currency: "$", currencyName: "USD", value: 0.3}));
+    it('100도 bottomBand만 적용되서 3 ', () =>
+        expect(baseCharge(100)).to.eql({currency: "$", currencyName: "USD", value: 3}));
+    it('101부턴 middleband도 적용되서 3.05 ', () =>
+        expect(baseCharge(101)).to.eql({currency: "$", currencyName: "USD", value: 3.05}));
+    it('200도 middleband도 적용되서 3+5 ', () =>
+        expect(baseCharge(200)).to.eql({currency: "$", currencyName: "USD", value: 3+5}));
+    it('201부턴 highband 적용 : 8.07 ', () =>
+        expect(baseCharge(201)).to.eql({currency: "$", currencyName: "USD", value: 3+5+0.07}));
+});
