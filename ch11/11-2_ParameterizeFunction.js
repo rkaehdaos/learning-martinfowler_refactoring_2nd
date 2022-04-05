5ab337015d635a4f1ce2bfd893fe4293d3a3c69a
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-05 11:11:50 +0900

    [#11 2.함수 매개변수화하기] step3
    - 함수 호출하는 곳 모두에 적절한 리터럴 추가

--- ch11/11-2_ParameterizeFunction.js
+++ ch11/11-2_ParameterizeFunction.js
@@ -2,7 +2,7 @@ export function baseCharge(usage) {
     if (usage < 0) return usd(0);
     const amount =
         bottomBand(usage) * 0.03
-        + middleBand(usage) * 0.05
+        + middleBand(usage,100,200) * 0.05
         + topBand(usage) * 0.07;
     return usd(amount);
 }
