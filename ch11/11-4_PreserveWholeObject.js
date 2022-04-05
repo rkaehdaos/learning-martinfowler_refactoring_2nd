35aba6ec489b5ecf98976a635e69d2b35b0e648e
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-05 17:33:49 +0900

    [#11 4.객체통째로 넘기기] step4 : 호출자가 새 함수를 사용하게 하나씩 수정하며 테스트

--- ch11/11-4_PreserveWholeObject.js
+++ ch11/11-4_PreserveWholeObject.js
@@ -18,7 +18,7 @@ export function temperatureAlerts(aRoom, aPlan) {
     const alerts = [];
     const low = aRoom.daysTempRange.low;
     const high = aRoom.daysTempRange.high;
-    if (!aPlan.withinRange(low, high))
+    if (!aPlan.xxNewWithinRange(aRoom.daysTempRange))
         alerts.push("방 온도가 지정 범위를 벗어났습니다.");
     return alerts;
 }
