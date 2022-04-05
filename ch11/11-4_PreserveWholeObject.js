f19badee7194ceebc736cf4751e3c7229e520997
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-05 17:35:17 +0900

    [#11 4.객체통째로 넘기기] step5 : 원래 함수 인라인

--- ch11/11-4_PreserveWholeObject.js
+++ ch11/11-4_PreserveWholeObject.js
@@ -3,13 +3,9 @@ export class HeatingPlan {
         this._temperatureRange = temperatureRange;
     }
 
-    withinRange(bottom, top) {
-        return (bottom >= this._temperatureRange.low)
-            && (top <= this._temperatureRange.high);
-    }
-
     xxNewWithinRange(aNumberRange) {
-        return this.withinRange(aNumberRange.low, aNumberRange.high);
+        return (aNumberRange.low >= this._temperatureRange.low) &&
+            (aNumberRange.high <= this._temperatureRange.high);
     }
 }
 
