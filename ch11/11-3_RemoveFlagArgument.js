4e618e6bef59a73ad8a596f23e2ff500127ddecf
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-05 15:59:18 +0900

    [#11 3.플래그 인수 제거하기] step2:명시적함수호출로 대체

--- ch11/11-3_RemoveFlagArgument.js
+++ ch11/11-3_RemoveFlagArgument.js
@@ -4,7 +4,7 @@ export function deliveryDate(anOrder, isRush) {
 
 }
 
-function rushDeliveryDate(anOrder) {
+export function rushDeliveryDate(anOrder) {
     let deliveryTime;
     if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
     else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
@@ -12,7 +12,7 @@ function rushDeliveryDate(anOrder) {
     return anOrder.placedOn.plusDays(1 + deliveryTime);
 }
 
-function regularDeliveryDate(anOrder) {
+export function regularDeliveryDate(anOrder) {
     let deliveryTime;
     if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) deliveryTime = 2;
     else if (["ME", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
