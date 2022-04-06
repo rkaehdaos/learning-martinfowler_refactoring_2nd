03a2bca86142e89e70aa854627c284108251df31
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-06 12:29:46 +0900

    [#11 5.매개변수를 질의함수로 바꾸기] step2 - 이 매개변수를 참조하는 코드를 함수 호출로 바꾼다

--- ch11/11-5_ReplaceParameterWithQuery.js
+++ ch11/11-5_ReplaceParameterWithQuery.js
@@ -13,7 +13,7 @@ export class Order {
     }
 
     discountedPrice(basePrice, discountLevel) {
-        switch (discountLevel) {
+        switch (this.discountLevel) {
             case 1: return basePrice * 0.95;
             case 2: return basePrice * 0.9;
         }
