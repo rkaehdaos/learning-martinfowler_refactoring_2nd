c909c023c5475ffbe2138924c7d4e05218d0deb7
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-06 12:24:42 +0900

    [#11 5.매개변수를 질의함수로 바꾸기] 0-7.4임시변수질의함수바꾸기-6
    - 함수를 간소화하다보면 임시변수를 질의함수로 바꾸기를 적용할 떄가 많다
    - 이를 finalPrice에 적용
    
    step6
    - 기존 임시변수 삭제

--- ch11/11-5_ReplaceParameterWithQuery.js
+++ ch11/11-5_ReplaceParameterWithQuery.js
@@ -6,9 +6,6 @@ export class Order {
 
     get finalPrice() {
         const basePrice = this.quantity * this.itemPrice;
-        let discountLevel;
-        if (this.quantity > 100) discountLevel = 2;
-        else discountLevel = 1;
         return this.discountedPrice(basePrice, this.discountLevel2);
     }
     get discountLevel2() {
