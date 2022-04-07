d57988ec60ff98c75cf0f0c4f3720069e9b816e0
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-07 14:32:32 +0900

    [#11 10.명령 함수로 바꾸기]s5-[6.2함수인라인]-1-본문으로 교체

--- ch11/11-10_ReplaceCommandWithFunction.js
+++ ch11/11-10_ReplaceCommandWithFunction.js
@@ -7,7 +7,8 @@ class ChargeCalculator {
 }
 
 function charge(customer, usage, provider) {
-    return new ChargeCalculator(customer, usage, provider).charge(customer, usage, provider);
+    const baseCharge = customer.baseRate * usage;
+    return baseCharge + provider.connectionCharge;
 }
 
 
