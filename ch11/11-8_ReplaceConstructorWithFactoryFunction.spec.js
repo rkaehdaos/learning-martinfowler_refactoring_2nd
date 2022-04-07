2c8ead39ba784b9d6ac9f83b2f77953ac1389a3a
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-07 09:37:52 +0900

    [#11 8.생성자→팩터리함수] 예제

---
+++ ch11/11-8_ReplaceConstructorWithFactoryFunction.spec.js
@@ -0,0 +1,18 @@
+import {expect} from "chai";
+import {Employee} from "./11-8_ReplaceConstructorWithFactoryFunction.js";
+
+describe('11-8 생성자를 팩터리로 바꾸기 : Employee', () => {
+    it('주어진 이름의 Manager 생성 ', () => {
+        const candidate = new Employee("Ahn", "M");
+
+        expect(candidate.name ).to.equal("Ahn");
+        expect(candidate.type ).to.equal("Manager");
+    });
+
+    it('주어진 이름의 엔지니어 생성 ', () => {
+        const candidate = new Employee("Ahn", "E");
+
+        expect(candidate.name ).to.equal("Ahn");
+        expect(candidate.type ).to.equal("Engineer");
+    });
+});
