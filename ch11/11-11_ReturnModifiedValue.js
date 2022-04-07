7f70b95f3d9fe0d1c6e0bb64b802e3c9ddab3f6d
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-07 15:11:46 +0900

    [#11 11.명령 함수로 바꾸기] 3-반환값 가리킬 새로운 변수 선언-변수명 오버라이딩안되게

--- ch11/11-11_ReturnModifiedValue.js
+++ ch11/11-11_ReturnModifiedValue.js
@@ -8,11 +8,12 @@ export function calculateWithGpsSignal(points, totalMinutes) {
     return totalAscent / totalMinutes;
 
     function calculateAscent() {
+        let result = 0;
         for (let i = 1; i < points.length; i++) {
             const verticalChange = points[i].elevation - points[i - 1].elevation;
-            totalAscent += (verticalChange > 0) ? verticalChange : 0;
+            result += (verticalChange > 0) ? verticalChange : 0;
         }
-        return totalAscent;
+        return result;
     }
 
     function calculateTime() {}
