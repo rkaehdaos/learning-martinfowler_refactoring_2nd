7dfbe1f707b17acb60e5d7cac2af76ec7a5ec9b0
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-07 11:19:33 +0900

    [#11 9.함수 명령으로 바꾸기] 0. 건강보험 애플리케이션의 점수 계산 함수

---
+++ ch11/11-9_ReplaceFunctionWithCommand.spec.js
@@ -0,0 +1,52 @@
+import {expect} from 'chai';
+import {score, ScoringGuide} from "./11-9_ReplaceFunctionWithCommand.js";
+
+describe('11-9 함수를 명령으로 대체', () => {
+    it('흡연자가 낮은 state일 때 점수', () => {
+        const lowCertificationStateCandidate = {
+            originState: 4
+        };
+
+        const smokerMedicalExam = {
+            isSmoker: true
+        };
+
+        expect(score(lowCertificationStateCandidate, smokerMedicalExam, new ScoringGuide())).to.equal(-10);
+    });
+
+    it('흡연자가 높은 state일 때 점수', () => {
+        const highCertificationStateCandidate = {
+            originState: 5
+        };
+
+        const smokerMedicalExam = {
+            isSmoker: true
+        };
+
+        expect(score(highCertificationStateCandidate, smokerMedicalExam, new ScoringGuide())).to.equal(-5);
+    });
+
+    it('비흡연자가 낮은 state일 때 점수', () => {
+        const lowCertificationStateCandidate = {
+            originState: 4
+        };
+
+        const nonSmokerMedicalExam = {
+            isSmoker: false
+        };
+
+        expect(score(lowCertificationStateCandidate, nonSmokerMedicalExam, new ScoringGuide())).to.equal(-5);
+    });
+
+    it('비흡연자가 높은 state일 때 점수', () => {
+        const highCertificationStateCandidate = {
+            originState: 5
+        };
+
+        const nonSmokerMedicalExam = {
+            isSmoker: false
+        };
+
+        expect(score(highCertificationStateCandidate, nonSmokerMedicalExam, new ScoringGuide())).to.equal(0);
+    });
+});
