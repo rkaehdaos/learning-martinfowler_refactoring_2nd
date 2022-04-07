1ce96a4efb07b78e0313acc15a3366731a36e778
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-07 16:10:06 +0900

    [#11 12. 오류코드 예외로 바꾸기] 내가 만든 예제!-가장고려할 것!
    고려사항
    - 이 오류는 예상된 것인가?
    - `localShippingRules()`는 배송 규칙들이 `countryData`에 제대로 반영되어있다고 가정해도 되는가?
    - country 인수가 전역 데이터의 키와 일치하는 곳에서 가져오거나 검증을 받았는가?
    
    고려사항이 긍정적이면 이번 리팩터링을 적용할 준비가 되었다

---
+++ ch11/11-12_ReplaceErrorCodeWithException.js
@@ -0,0 +1,30 @@
+let countryData = {};
+countryData.shippingRules = {};
+countryData.shippingRules["서울"] = "퀵";
+countryData.shippingRules["인천"] = "퀵";
+countryData.shippingRules["수원"] = "퀵";
+countryData.shippingRules["부산"] = "택배";
+countryData.shippingRules["제주"] = "택배";
+
+
+function localShippingRules(country) {
+    const data = countryData.shippingRules[country];
+    if (data) return new ShippingRules(data);
+    else return -23;
+}
+
+function calculateShippingCosts(anOrder) {
+    // 관련 없는 코드
+    const shippingRules = localShippingRules(anOrder.country);
+    if(shippingRules<0) return shippingRules;
+    //더 관련 없는 룰
+}
+
+export function calcaulateTop(orderData, errorList) {
+    const status = calculateShippingCosts(orderData);
+    if (status<0) errorList.push({order: orderData, errorCode: status});
+}
+
+class ShippingRules {
+    constructor(data) {}
+}
