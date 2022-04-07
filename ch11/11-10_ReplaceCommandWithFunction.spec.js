6af6326d830a9d66caa3963ed66d110303fd93c9
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-07 14:22:07 +0900

    [#11 10.명령 함수로 바꾸기] 0-테스트 변경
    - 중간값 테스트 삭제

--- ch11/11-10_ReplaceCommandWithFunction.spec.js
+++ ch11/11-10_ReplaceCommandWithFunction.spec.js
@@ -5,12 +5,6 @@ describe('11-10 명령객체를 함수로 ', () => {
     const customer = {baseRate: 0.1};
     const provider = {connectionCharge: 1000};
 
-    it('baseCharge 계산', () => {
-        const chargeCalculator = new ChargeCalculator(customer, 100, provider);
-
-        expect(chargeCalculator.baseCharge).to.equal(10);
-    });
-
     it('전체 charge 계산', () => {
         const chargeCalculator = new ChargeCalculator(customer, 100, provider);
 
