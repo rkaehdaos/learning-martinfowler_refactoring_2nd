945b7e0c4dc9b1e3c899e5843c9fae536652e918
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-07 14:23:39 +0900

    [#11 10.명령 함수로 바꾸기] step1-1! - 호출자 변경

--- ch11/11-10_ReplaceCommandWithFunction.spec.js
+++ ch11/11-10_ReplaceCommandWithFunction.spec.js
@@ -1,14 +1,13 @@
 import {expect} from "chai";
-import {ChargeCalculator} from "./11-10_ReplaceCommandWithFunction.js";
+import {ChargeCalculator,charge} from "./11-10_ReplaceCommandWithFunction.js";
 
 describe('11-10 명령객체를 함수로 ', () => {
     const customer = {baseRate: 0.1};
     const provider = {connectionCharge: 1000};
 
     it('전체 charge 계산', () => {
-        const chargeCalculator = new ChargeCalculator(customer, 100, provider);
-
-        expect(chargeCalculator.charge).to.equal(1010);
+        const monthCharge = charge(customer, 100, provider);
+        expect(monthCharge).to.equal(1010);
     });
 
 });
