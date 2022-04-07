adf5a1f2e223c790a7a33e5d4c749dc82e382250
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-07 09:41:15 +0900

    [#11 8.생성자→팩터리함수] 2. 생성자 호출하는 곳 수정

--- ch11/11-8_ReplaceConstructorWithFactoryFunction.spec.js
+++ ch11/11-8_ReplaceConstructorWithFactoryFunction.spec.js
@@ -1,16 +1,16 @@
 import {expect} from "chai";
-import {Employee} from "./11-8_ReplaceConstructorWithFactoryFunction.js";
+import {Employee,createEmployee} from "./11-8_ReplaceConstructorWithFactoryFunction.js";
 
 describe('11-8 생성자를 팩터리로 바꾸기 : Employee', () => {
     it('주어진 이름의 Manager 생성 ', () => {
-        const candidate = new Employee("Ahn", "M");
+        const candidate = createEmployee("Ahn", "M");
 
         expect(candidate.name ).to.equal("Ahn");
         expect(candidate.type ).to.equal("Manager");
     });
 
     it('주어진 이름의 엔지니어 생성 ', () => {
-        const candidate = new Employee("Ahn", "E");
+        const candidate = createEmployee("Ahn", "E");
 
         expect(candidate.name ).to.equal("Ahn");
         expect(candidate.type ).to.equal("Engineer");
