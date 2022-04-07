21e59a65523c918373b04284d2299c0bed9d47ec
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-07 15:00:00 +0900

    [#11 11.명령 함수로 바꾸기] 0-예시:GPS 신호로 계산하기

---
+++ ch11/11-11_ReturnModifiedValue.js
@@ -0,0 +1,19 @@
+export function calculateWithGpsSignal() {
+    let totalAscent = 0;
+    let totalTime = 0;
+    let totalDistance = 0;
+    calculateAscent();
+    calculateTime();
+    calculateDistance();
+    const pace = totalTime / 60 / totalDistance;
+
+    function calculateAscent() {
+
+    }
+    function calculateTime() {
+
+    }
+    function calculateDistance() {
+
+    }
+}
