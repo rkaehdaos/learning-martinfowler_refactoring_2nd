751f854100ab9b8365acdb36038e9a47bd9f2824
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-07 14:28:19 +0900

    [#11 10.명령 함수로 바꾸기] step4! 하나씩-1
    - 명령의 실행 메서드에서 필드 대신 매개변수 사용하도록 바꾼다. 하나씩 수정할 떄 마다 테스트

--- ch11/11-10_ReplaceCommandWithFunction.js
+++ ch11/11-10_ReplaceCommandWithFunction.js
@@ -1,11 +1,10 @@
 class ChargeCalculator {
     constructor (customer, usage, provider){
-        this._customer = customer;
         this._usage = usage;
         this._provider = provider;
     }
     charge(customer, usage, provider) {
-        const baseCharge = this._customer.baseRate * this._usage;
+        const baseCharge = customer.baseRate * this._usage;
         return baseCharge + this._provider.connectionCharge;
     }
 }
