97c1e2ab036c7bd24055b2e33ccdc21ef2099fe3
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-08 15:02:02 +0900

    [#12 3.생성자 본문 올리기] 2. 공통 문장 모두 super()직후로 옮긴다

--- ch12/12-3_PullUpConstructorBody.js
+++ ch12/12-3_PullUpConstructorBody.js
@@ -6,8 +6,8 @@ class Party {
 class Employee extends Party {
     constructor(name, id, monthlyCost) {
         super();
-        this._id = id;
         this._name = name;
+        this._id = id;
         this._monthlyCost = monthlyCost;
     }
     get id() {return this._id;}
