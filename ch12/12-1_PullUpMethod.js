998021e926c493719582682cbf5ab4d34ee92fe6
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-08 11:53:54 +0900

    [#12 1.메서드 올리기] 서브클래스 책임 오류를 위한 함정 메서드 구현

--- ch12/12-1_PullUpMethod.js
+++ ch12/12-1_PullUpMethod.js
@@ -5,12 +5,22 @@ export class Party {
     get annualCost() {
         return this.monthlyCost * 12;
     }
+
+    // 함정 메서드
+    get monthlyCost() {
+        throw new Error("서브클래스가 책임질 부분!");
+    }
+
 }
 
 export class Employee extends Party {
     constructor(monthlyCost) {
         super();
-        this.monthlyCost = monthlyCost;
+        this._monthlyCost = monthlyCost;
+    }
+
+    get monthlyCost() {
+        return this._monthlyCost;
     }
 
 }
@@ -18,7 +28,11 @@ export class Employee extends Party {
 export class Department extends Party {
     constructor(monthlyCost) {
         super();
-        this.monthlyCost = monthlyCost;
+        this._monthlyCost = monthlyCost;
+    }
+
+    get monthlyCost() {
+        return this._monthlyCost;
     }
 
 }
