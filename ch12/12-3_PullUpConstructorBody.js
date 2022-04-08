9c1a690ea0a915fe1a77b47eed107e5e19166f6d
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-08 15:00:21 +0900

    [#12 3.생성자 본문 올리기] 어디서 많이 본 예제

---
+++ ch12/12-3_PullUpConstructorBody.js
@@ -0,0 +1,25 @@
+class Party {
+}
+
+class Employee extends Party {
+    constructor(name, id, monthlyCost) {
+        super();
+        this._id = id;
+        this._name = name;
+        this._monthlyCost = monthlyCost;
+    }
+    get id() {return this._id;}
+    get name() {return this._name;}
+    get monthlyCost() {return this._monthlyCost;}
+}
+
+class Department extends Party {
+    constructor(name, staff) {
+        super();
+        this._name = name;
+        this._staff = staff;
+    }
+    get staff() {return this._staff;}
+}
+
+export {Party, Employee, Department};
