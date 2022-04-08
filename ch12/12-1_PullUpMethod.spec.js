7f4c6d041878ffa9838a9e24ec6a5610e3b80c70
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-08 11:42:18 +0900

    [#12 1.메서드 올리기] s1, s2!~
    - 테스트 추가
    - s1: 똑같이 동작하는 메서드인지 면밀히 체크
    - s2: monthlyCost는 슈퍼 클래스에 없음
      - 동적언어인 자바스크립트는 그대로 괜춘
      - 정적 언어라면 슈퍼클래스에 추상 메서드 정의 필요

--- ch12/12-1_PullUpMethod.spec.js
+++ ch12/12-1_PullUpMethod.spec.js
@@ -2,10 +2,12 @@ import {expect} from "chai";
 import {Party, Employee, Department} from "./12-1_PullUpMethod.js";
 
 describe('12-1 메서드 올리기', () => {
-    it('1', () => {
+    it('Employee', () => {
         const employee = new Employee(10);
-        const department = new Department(20);
         expect(employee.annualCost).to.equal(120);
+    });
+    it('Department', () => {
+        const department = new Department(20);
         expect(department.totalAnnualCost).to.equal(240);
     });
 });
