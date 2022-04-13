f843b6be00803f793a22335ba7a00a06df6f458d
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-13 15:33:27 +0900

    [#12 6.typeCode를 서브클래스로] s6-타입코드필드제거,슈퍼클래스 게터 제거

--- ch12/12-6_ReplaceTypeCodeWithSubclasses.js
+++ ch12/12-6_ReplaceTypeCodeWithSubclasses.js
@@ -3,15 +3,13 @@ export class Employee {
     constructor(name, type) {
         this.validateType(type);
         this._name = name;
-        this._type = type;
+
     }
 
     validateType(arg) {
         if (!["engineer", "manager", "salesman"].includes(arg))
             throw new Error(`${arg}라는 직원 유형은 없습니다.`);
     }
-
-    get type() {return this._type;}
 
     toString() {
         return `${this._name} (${this.type})`;
