29453ef6c1e8387418e2c8ae2abf3733bf45d177
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-13 15:28:22 +0900

    [#12 6.typeCode를 서브클래스로] s4-팩포리메소드 사용 및 테스트

--- ch12/12-6_ReplaceTypeCodeWithSubclasses.spec.js
+++ ch12/12-6_ReplaceTypeCodeWithSubclasses.spec.js
@@ -1,28 +1,28 @@
 import {expect} from 'chai';
-import {Employee} from "./12-6_ReplaceTypeCodeWithSubclasses.js";
+import {Employee,createEmployee} from "./12-6_ReplaceTypeCodeWithSubclasses.js";
 
 
 describe('12-6 타입코드를 서브클래스로', () => {
     it('create engineer', () => {
-        const engineer = new Employee("Tom", "engineer");
+        const engineer = createEmployee("Tom", "engineer");
 
         expect(engineer.toString()).to.equal("Tom (engineer)");
     });
 
     it('create manager', () => {
-        const engineer = new Employee("Sarah", "manager");
+        const engineer = createEmployee("Sarah", "manager");
 
         expect(engineer.toString()).to.equal("Sarah (manager)");
     });
 
     it('create salesman', () => {
-        const engineer = new Employee("David", "salesman");
+        const engineer = createEmployee("David", "salesman");
 
         expect(engineer.toString()).to.equal("David (salesman)");
     });
 
     it('그외의 타입코드는 에러 ', () => {
-        expect(function(){ new Employee("Ahn", "developer")}).to.throw("developer라는 직원 유형은 없습니다.");
+        expect(function(){ createEmployee("Ahn", "developer")}).to.throw("developer라는 직원 유형은 없습니다.");
     });
 
 });
