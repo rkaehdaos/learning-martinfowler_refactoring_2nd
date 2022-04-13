f0d36e621d7c7ac7a85b23347470aaf967c14c2b
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-13 15:32:10 +0900

    [#12 6.typeCode를 서브클래스로] s5 - Manager

--- ch12/12-6_ReplaceTypeCodeWithSubclasses.js
+++ ch12/12-6_ReplaceTypeCodeWithSubclasses.js
@@ -25,12 +25,19 @@ class Engineer extends Employee {
 class Salesman extends Employee {
     get type() {return "salesman";}
 }
+
+class Manager extends Employee {
+    get type() {return "manager";}
+}
+
 export function createEmployee(name, type) {
     switch (type) {
         case "engineer":
             return new Engineer(name, type);
         case "salesman":
             return new Salesman(name, type);
+        case "manager":
+            return new Manager(name, type);
     }
     return new Employee(name, type);
 }
