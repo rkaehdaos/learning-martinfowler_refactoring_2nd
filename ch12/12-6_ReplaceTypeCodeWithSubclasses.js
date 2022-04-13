07c4acdd11bce2b579d9ddecf09ca8556c0b14fc
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-04-13 16:17:50 +0900

    [#12 6.typeCode를 서브클래스로] s8 - [12.4 메서드 내리기]

--- ch12/12-6_ReplaceTypeCodeWithSubclasses.js
+++ ch12/12-6_ReplaceTypeCodeWithSubclasses.js
@@ -4,21 +4,21 @@ export class Employee {
         this._name = name;
     }
 
-    toString() {
-        return `${this._name} (${this.type})`;
-    }
 }
 
 class Engineer extends Employee {
     get type() {return "engineer";}
+    toString() {return `${this._name} (engineer)`;}
 }
 
 class Salesman extends Employee {
     get type() {return "salesman";}
+    toString() {return `${this._name} (salesman)`;}
 }
 
 class Manager extends Employee {
     get type() {return "manager";}
+    toString() {return `${this._name} (manager)`;}
 }
 
 export function createEmployee(name, type) {
